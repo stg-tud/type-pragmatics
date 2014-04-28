a0b75fd5c068df4b6193d4b7ba1c4efc75b33664
Sebastian Erdweg <seba.github@gmail.com>
2014-04-28 14:56:16 +0200

    support equality judgements; variables introduced in conclusion must be existentially bound

--- pascal-thesis/SimplyTypedLambdaCalculus/trans/fof-rewriting.pp
+++ pascal-thesis/SimplyTypedLambdaCalculus/trans/fof-rewriting.pp
@@ -5,23 +5,31 @@
 	Conjecture -- "conjecture",
 	All -- "![" _1 "] : (" _2 ")",
 	All.1:iter-star-sep -- _1 ",",
+	Exists -- "?[" _1 "] : (" _2 ")",
+	Exists.1:iter-star-sep -- _1 ",",
 	Impl -- "(" _1 " => " _2 ")",
+	Neq -- "(" _1 "!=" _2 ")",
 	Pred -- _1 "(" _2 ")",
 	Pred.2:iter-star-sep -- _1 ",",
 	Abs -- "abs("_1 "," _2 "," _3 ")",
 	App -- "app(" _1 "," _2 ")",
-	Fork -- "fork(" _1 ")",
-	Join -- "join(" _1 ")",
 	TyNat -- "nat",
+	TyVar -- _1,
 	TyFuture --  "future(" _1 ")",
 	TyFunction -- "arrow(" _1 "," _2 ")",
-	PremiseCons -- "(" _1 " & " _2 ")",
+	JudgementCons -- "(" _1 " & " _2 ")",
+	EqualityJudgement -- "(" _1 " = " _2 ")",
 	
 	MetaVariable -- _1,
 	Meta -- _1,
 	Var -- _1,
 	PremiseList -- _1,
+	ConsequenceList -- _1,
 	MetaVariable -- _1,
 	
-	Nat -- _1
+	Nat -- _1,
+	
+	Fork -- "fork(" _1 ")",
+	Join -- "join(" _1 ")",
+	Par -- "par(" _1 ")"
 ]
