7a9731fb505d056a60abc2491334902af09de36a
Sebastian Erdweg <seba.github@gmail.com>
2014-04-28 13:08:01 +0200

    fine-tune pp

--- pascal-thesis/SimplyTypedLambdaCalculus/trans/fof-rewriting.pp
+++ pascal-thesis/SimplyTypedLambdaCalculus/trans/fof-rewriting.pp
@@ -1,11 +1,11 @@
 [
 	File -- V [ _1 _2 ],
-	Formula -- H hs=1 ["fof('" _1 "'," _2 "," _3 ")."],
+	Formula -- H hs=0 ["fof('" _1 "', " _2 ", " _3 ")."],
 	Axiom -- "axiom",
 	Conjecture -- "conjecture",
 	All -- "![" _1 "] : (" _2 ")",
 	All.1:iter-star-sep -- _1 ",",
-	Impl -- "(" _1 "=>" _2 ")",
+	Impl -- "(" _1 " => " _2 ")",
 	Pred -- _1 "(" _2 ")",
 	Pred.2:iter-star-sep -- _1 ",",
 	Abs -- "abs("_1 "," _2 "," _3 ")",
@@ -15,7 +15,7 @@
 	TyNat -- "nat",
 	TyFuture --  "future(" _1 ")",
 	TyFunction -- "arrow(" _1 "," _2 ")",
-	PremiseCons -- "(" _1 "&" _2 ")",
+	PremiseCons -- "(" _1 " & " _2 ")",
 	
 	MetaVariable -- _1,
 	Meta -- _1,
