a2020d65b008567f753229ea713d5c48a379327f
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-08-26 17:19:02 +0200

    removed superfluous TODO

--- scala-backend/src/de/tu_darmstadt/veritas/backend/tff/TypedVariable.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/tff/TypedVariable.scala
@@ -5,5 +5,4 @@ import de.tu_darmstadt.veritas.backend.fof.Variable
 
 final case class TypedVariable(name: String, tfftype: TffAtomicType) extends Variable(name) with SimplePrettyPrintable {
   override def prettyString = super.prettyString + ": " + tfftype.prettyString
-  // TODO add pretty String for TffType
 }
