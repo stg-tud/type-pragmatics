dcc7428ea2deed8b93f3c4bdc5584d55bde442ca
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-08-27 16:23:57 +0200

    refined tff transformation for axioms and goals
    not yet complete!

--- scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/ToTff.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/ToTff.scala
@@ -16,6 +16,11 @@ import de.tu_darmstadt.veritas.backend.veritas._
 object ToTff {
 
   /**
+   * list for collecting type declarations from constructor/function declarations in Module
+   */
+  private var typedecllist: Seq[TffAnnotated] = Seq()
+
+  /**
    * list for collecting the axioms in the module
    */
   private var axiomlist: Seq[TffAnnotated] = Seq()
@@ -32,7 +37,7 @@ object ToTff {
           bodyToTff(body)
           constructFinalTff(name)
         } catch {
-          case TransformationError(s) => throw TransformationError(s"Module ${name} was to be transformed to TFF, but contained elements not supported by core modules!")
+          case TransformationError(s) => throw TransformationError(s"Module ${name} was to be transformed to TFF, but contained elements not supported by core modules:" + s)
         }
       }
       case Module(name, _, _) => throw TransformationError(s"Module ${name} was to be transformed to TFF, but still contained imports!")
@@ -42,21 +47,46 @@ object ToTff {
   private def bodyToTff(body: Seq[ModuleDef]): Unit = {
     body foreach { md =>
       md match {
-        case Axioms(axs)      => ???
-        case Goals(gs, _)     => ???
+        case Axioms(axs) => axiomlist ++ translateAxioms(axs)
+        case Goals(gs, _) => if (goal != None | gs.length > 1)
+          throw TransformationError("More than one goal found!")
+        else goal = Some(translateGoal(gs(0)))
         case Constructors(cs) => ???
         case Consts(cs)       => ???
         case Sorts(s)         => ???
         case Functions(fds)   => ???
-        case _                => throw TransformationError("unsupported construct!")
+        case _                => throw TransformationError("Unsupported top-level construct!")
 
       }
     }
   }
 
+  private def translateAxioms(axs: Seq[TypingRule]): Seq[TffAnnotated] =
+    for (a <- axs)
+      yield a match {
+      case TypingRule(name, prems, conseqs) =>
+        TffAnnotated(name, Axiom, typingRuleToTff(prems, conseqs))
+    }
+  
+  private def translateGoal(g: TypingRule): TffAnnotated = 
+    g match {
+    case TypingRule(name, prems, conseqs) =>
+      TffAnnotated(name, Conjecture, typingRuleToTff(prems, conseqs))
+  }
+  
+  private def typingRuleToTff(prems: Seq[TypingRuleJudgment], conseqs: Seq[TypingRuleJudgment]) =
+    Impl(makeAnd(prems map jdgtoTff), makeAnd(conseqs map jdgtoTff))
+
+  
+  
+  private def jdgtoTff(jdg: TypingRuleJudgment): FofUnitary = ???
+  
+  private def makeAnd(formulas: Seq[FofUnitary]): FofUnitary = ???
+  
+
   private def constructFinalTff(name: String): TffFile = {
     goal match {
-      case Some(g) => TffFile(name, axiomlist ++ Seq(g))
+      case Some(g) => TffFile(name, typedecllist ++ axiomlist ++ Seq(g))
       case None    => throw TransformationError(s"There was no goal in Module ${name}; TFF Transformation failed!")
     }
   }
