d858c777f3fe2c0a74ab9ba80b8ded7c4820b879
Daniel Lehmann <daniel.lehmann@stud.tu-darmstadt.de>
2015-09-01 16:57:42 +0200

    changed Fof case classes to normal classes with companion object apply that does simplification based on the inputs (similar to make-* in props.str)

--- scala-backend/src/de/tu_darmstadt/veritas/backend/fof/FofBinaryAssoc.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/fof/FofBinaryAssoc.scala
@@ -3,7 +3,7 @@ package de.tu_darmstadt.veritas.backend.fof
 import de.tu_darmstadt.veritas.backend.util.prettyprint.PrettyPrintWriter
 
 sealed abstract class FofBinaryAssoc(args: Seq[FofUnitary], opString: String) extends Fof {
-  require(args.size >= 2, "binary op with less than 2 arguments is useless")
+  require(args.size >= 2, "And/Or should never be created with less than 2 arguments, see the companion objects")
   
   override def prettyPrint(writer: PrettyPrintWriter) = {
     writer.write(args.head)
@@ -14,5 +14,36 @@ sealed abstract class FofBinaryAssoc(args: Seq[FofUnitary], opString: String) ex
   }
 }
 
-final case class Or(args: Seq[FofUnitary]) extends FofBinaryAssoc(args, " | ")
-final case class And(args: Seq[FofUnitary]) extends FofBinaryAssoc(args, " & ")
+
+final class Or private(val args: Seq[FofUnitary]) extends FofBinaryAssoc(args, " | ") {
+  override def toString = args.mkString("Or(", ", ", ")")
+}
+object Or {
+  def apply(args: Seq[FofUnitary]): Fof = args match {
+    case Seq() => False
+    case Seq(single) => single
+    case elems if elems.contains(True) => True
+    case elems => Or(elems.filterNot { 
+      case False => true
+      case _ => false
+    })
+  }
+  def unapply(e: Or): Option[Seq[FofUnitary]] = Some(e.args)
+}
+
+
+final class And private (val args: Seq[FofUnitary]) extends FofBinaryAssoc(args, " & ") {
+  override def toString = args.mkString("And(", ", ", ")")
+}
+object And {
+  def apply(args: Seq[FofUnitary]): Fof = args match {
+    case Seq() => True
+    case Seq(single) => single
+    case elems if elems.contains(False) => False
+    case elems => And(elems.filterNot { 
+      case True => true
+      case _ => false
+    })
+  }
+  def unapply(e: And): Option[Seq[FofUnitary]] = Some(e.args)
+}
