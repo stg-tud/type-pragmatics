7283b5dc89adbbb782b31e7b98fe3a6c8fd5fd0c
Daniel Lehmann <daniel.lehmann@stud.tu-darmstadt.de>
2015-09-03 17:26:22 +0200

    refactor ToFof: now assumes Core TSSL as input (no imports/TypingJudgments etc.), uses FreeVariables now

--- scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/ToFof.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/ToFof.scala
@@ -5,103 +5,98 @@ import de.tu_darmstadt.veritas.backend.util.BackendError
 import de.tu_darmstadt.veritas.backend.veritas._
 import de.tu_darmstadt.veritas.backend.util.prettyprint.PrettyPrintable
 import de.tu_darmstadt.veritas.backend.util.prettyprint.PrettyPrintWriter
+import de.tu_darmstadt.veritas.backend.util.FreeVariables
+import de.tu_darmstadt.veritas.backend.fof.Variable
 
-// TODO refactor and do proper modular subdivisions
+/**
+ * Transforms Core TSSL (Veritas) Modules to FOF syntax
+ *
+ * Structure of Core Modules
+ * - no imports
+ * - section with "symbol declarations" (constructor decls, const decls, function sigs...) (can be empty)
+ * - section with n axioms, where typing judgments were already transformed to some typed function! (can be empty)
+ * - exactly one goal!
+ */
 object ToFof {
-  def toFofFiles(veritasModule: Module): Seq[FofFile] = {
-    val formulas = toFofFormulas(veritasModule)
+  def toFofFile(coreModule: Module): FofFile = coreModule match {
+    case Module(name, Seq(), body) => {
+      val goal = getOnlyGoal(coreModule)
+      val axioms = coreModule.body.collect{ case Axioms(axioms) => axioms }.flatten
 
-    // divide into conjectures and everything else (TODO only axioms?)
-    val isConjectureMap = formulas groupBy (_.role == Conjecture)
-    val conjectures = isConjectureMap.getOrElse(true, Nil)
-    val notConjectures = isConjectureMap.getOrElse(false, Nil)
+      val transformedAxioms = axioms map (typingRuleToFof(_, Axiom))
+      val transformedGoal = typingRuleToFof(goal, Conjecture)
+      
+      FofFile(name + ".fof", transformedAxioms :+ transformedGoal)
+    }
+    case Module(name, _, _) => throw TransformationError(s"(Core TSSL) Module $name still contained imports")
+  }
 
-    // create one file per conjecture
-    for (conjectureFormula <- conjectures) yield {
-      val inputModule = veritasModule.name.split('.').last
-      FofFile(inputModule + "-" + conjectureFormula.name + ".fof", notConjectures :+ conjectureFormula)
+  /**
+   * returns the single goal, throws TransformationError if
+   *  - there is no goal (every .fof file must have one for proof)
+   *  - there is more than one (fof allows only one per file)
+   */
+  private def getOnlyGoal(mod: Module): TypingRule = {
+    val allGoals = mod.body.collect{ case Goals(goals, /* TODO */ timeout) => goals }.flatten 
+    allGoals match {
+      case Seq() => throw TransformationError(s"(Core TSSL) Module ${mod.name} contained no goal")
+      case Seq(singleGoal) => singleGoal
+      case _ => throw TransformationError(s"(Core TSSL) Module ${mod.name} contained more than one goal")
     }
   }
-
-  private def toFofFormulas(veritasModule: Module): Seq[FofAnnotated] = {
-    // TODO put this in an optimization pass
-    // collect all goals and axioms into one list each (such that they are not scattered over different ModuleDefs)
-    val collectedGoals = veritasModule.body.collect({ case Goals(goals, _) => goals }).flatten
-    val collectedAxioms = veritasModule.body.collect({ case Axioms(axioms) => axioms }).flatten
-
-    val goals = collectedGoals map (ToFof.toFofAnnotated(_, Conjecture))
-    val axioms = collectedAxioms map (ToFof.toFofAnnotated(_, Axiom))
-
-    goals ++ axioms
+  
+  private def typingRuleToFof(rule: TypingRule, role: FormulaRole): FofAnnotated =
+    FofAnnotated(rule.name, role, typingRuleToFof(rule.premises, rule.consequences))
+  
+  private def typingRuleToFof(prems: Seq[TypingRuleJudgment], conseqs: Seq[TypingRuleJudgment]): Fof = {
+    val quantifiedVars = FreeVariables.freeVariables(prems ++ conseqs) map toUntypedVar
+    ForAll(quantifiedVars.toSeq, Parenthesized( 
+        Impl(Parenthesized(And(prems map jdgToFof)), Parenthesized(And(conseqs map jdgToFof)))))
   }
 
-  // TODO idiomatic Scala way for free functions
-  private def toFofAnnotated(rule: TypingRule, typ: FormulaRole): FofAnnotated = {
-    val usedVariableNames = (rule.premises ++ rule.consequences).flatMap(CollectVariables.allVariables).distinct
-    val quantifiedVariables = usedVariableNames map (UntypedVariable(_))
-
-    // premises are ANDed, consequences ORed
-    // TODO put this in an optimization pass: simplify And(oneFormula) => oneFormula
-    val premiseFormulas: FofUnitary = rule.premises match {
-      case Seq()              => True
-      case Seq(premise)       => ToFof.toFof(premise)
-      case premises @ Seq(_*) => Parenthesized(And(premises map ToFof.toFof))
+  /**
+   * translates individual clauses (premises or conclusion)
+   */
+  private def jdgToFof(jdg: TypingRuleJudgment): FofUnitary =
+    jdg match {
+      case FunctionExpJudgment(f)        => functionExpToFof(f)
+      case ExistsJudgment(vars, jdglist) => Exists(vars map toUntypedVar, Parenthesized(And(jdglist map jdgToFof)))
+      case ForallJudgment(vars, jdglist) => ForAll(vars map toUntypedVar, Parenthesized(And(jdglist map jdgToFof)))
+      case NotJudgment(jdg)              => Not(jdgToFof(jdg))
+      case OrJudgment(ors)               => Parenthesized(Or(ors map (orcase => Parenthesized(And(orcase map jdgToFof)))))
+      case _                             => throw TransformationError("Encountered unsupported (not Core) judgment while translating a goal or axiom (e.g. typing judgment): " + jdg)
     }
-    val consequenceFormulas: FofUnitary = rule.consequences match {
-      // TODO goal without consequences ever useful?
-      case Seq()                  => throw BackendError("Goal without consequences (list of consequences is empty)")
-      case Seq(consequence)       => ToFof.toFof(consequence)
-      case consequences @ Seq(_*) => Parenthesized(Or(consequences map ToFof.toFof))
+  
+  private def toUntypedVar(v: MetaVar): UntypedVariable = UntypedVariable(v.name)
+
+  /**
+   * translate individual function expressions
+   * outer function expressions cannot be MetaVars, since a MetaVar cannot be translated to a FofUnitary
+   */
+  private def functionExpToFof(f: FunctionExp): FofUnitary =
+    f match {
+      case FunctionExpNot(f)            => Not(functionExpToFof(f))
+      case FunctionExpEq(f1, f2)        => Eq(functionExpMetaToFof(f1), functionExpMetaToFof(f2))
+      case FunctionExpNeq(f1, f2)       => NeqEq(functionExpMetaToFof(f1), functionExpMetaToFof(f2))
+      case FunctionExpAnd(l, r)         => Parenthesized(And(Seq(functionExpToFof(l), functionExpToFof(r))))
+      case FunctionExpOr(l, r)          => Parenthesized(Or(Seq(functionExpToFof(l), functionExpToFof(r))))
+      case FunctionExpBiImpl(l, r)      => Parenthesized(BiImpl(functionExpToFof(l), functionExpToFof(r)))
+      case FunctionExpApp(n, args @ _*) => Appl(UntypedFunSymbol(n), (args map functionExpMetaToFof): _*)
+      case FunctionExpTrue              => True
+      case FunctionExpFalse             => False
+      case _                            => throw TransformationError("Encountered unsupported (not Core) function expression while translating (e.g. if or let expression): " + f)
     }
 
-    FofAnnotated(rule.name, typ, ForAll(quantifiedVariables, Parenthesized(Impl(premiseFormulas, consequenceFormulas))))
-  }
-
-  // TODO make these methods more general
-  private def toFof(rule: TypingRuleJudgment): FofUnitary = rule match {
-    case TypingJudgment(f1, f2, f3) => Appl(UntypedFunSymbol("tcheck"), toFof(f1), toFof(f2), toFof(f3))
-    case TypingJudgmentSimple(f1, f2) => Appl(UntypedFunSymbol("tchecksimple"), toFof(f1), toFof(f2))
-    case FunctionExpJudgment(FunctionExpEq(f1, f2)) => Eq(toFof(f1), toFof(f2))
-    case _ => throw BackendError("toFof(TypingRuleJudgement)")
-  }
-
-  private def toFof(exp: FunctionExpMeta): Term = exp match {
-    case FunctionMeta(MetaVar(metavar)) => UntypedVariable(metavar)
-    // FIXME how to ensure that "FunctionExp" will always only give Terms, not new Formulas?
-    // case FunctionExpEq(f1, f2) => Variable("TODO eq")
-    case _                              => throw BackendError("toFof(FunctionExp)")
-  }
-}
-
-private object CollectVariables {
-  // FIXME scala, which is more idiomatic?
-  // a) implement method on trait with case class match
-  // b) abstract method on trait, implement for each subclass
-  // or c) put this method in toTptp pass, not here
-
-  // TODO change from allVariables to freeVariables -> "does also do a typecheck"
-  // FIXME Contract: may contain duplicates!
-  def allVariables(rule: TypingRuleJudgment): Seq[String] = rule match {
-    case TypingJudgment(f1, f2, f3)   => allVariables(f1) ++ allVariables(f2) ++ allVariables(f3)
-    case TypingJudgmentSimple(f1, f2) => allVariables(f1) ++ allVariables(f2)
-    case FunctionExpJudgment(f)       => allVariables(f)
-    case _                            => List()
-  }
-
-  // FIXME String for variables is not typesafe...
-  def allVariables(exp: FunctionExpMeta): Seq[String] = exp match {
-    case FunctionMeta(MetaVar(metavar))     => List(metavar)
-    case FunctionExpNot(f)                  => allVariables(f)
-    case FunctionExpEq(f1, f2)              => allVariables(f1) ++ allVariables(f2)
-    case FunctionExpNeq(f1, f2)             => allVariables(f1) ++ allVariables(f2)
-    case FunctionExpOr(f1, f2)              => allVariables(f1) ++ allVariables(f2)
-    case FunctionExpBiImpl(f1, f2)          => allVariables(f1) ++ allVariables(f2)
-    case FunctionExpAnd(f1, f2)             => allVariables(f1) ++ allVariables(f2)
-    case FunctionExpIf(c, f1, f2)           => allVariables(c) ++ allVariables(f1) ++ allVariables(f2)
-    case FunctionExpLet(_, f1, f2)          => allVariables(f1) ++ allVariables(f2)
-    case FunctionExpApp(_, args @ _*)       => args.flatMap(allVariables)
-    case FunctionExpTrue | FunctionExpFalse => Nil
-    // FIXME how is FunctionExpVar to handle?
-    case _: FunctionExpVar                  => Nil
-  }
+  /**
+   * translate function expressions including MetaVars to terms
+   */
+  private def functionExpMetaToFof(f: FunctionExpMeta): Term =
+    // the only two constructs which can be turned into a term are
+    // FunctionMeta and FunctionExpApp (Appl is both a Term and a FofUnitary!)
+    // therefore, encountering any other FunctionExpMeta must result in an error!
+    f match {
+      case FunctionMeta(MetaVar(m))     => UntypedVariable(m)
+      case FunctionExpApp(n, args @ _*) => Appl(UntypedFunSymbol(n), (args map functionExpMetaToFof): _*)
+      case _                            => throw TransformationError("Encountered unexpected construct in functionExpMetaToTff: " + f)
+    }
 }
