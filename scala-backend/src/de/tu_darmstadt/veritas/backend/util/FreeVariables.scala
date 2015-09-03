3fb668b23560bdb551a2d0d919165449453dc056
Daniel Lehmann <daniel.lehmann@stud.tu-darmstadt.de>
2015-09-03 17:25:09 +0200

    added comments to FreeVariables, slight API change: ignore list is now Set

--- scala-backend/src/de/tu_darmstadt/veritas/backend/util/FreeVariables.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/util/FreeVariables.scala
@@ -4,10 +4,17 @@ import de.tu_darmstadt.veritas.backend.veritas._
 import de.tu_darmstadt.veritas.backend.veritas.function._
 
 object FreeVariables {
-  def freeVariables(jdgmnts: Seq[TypingRuleJudgment], ignore: Seq[MetaVar] = Seq()): Set[MetaVar]
+  /**
+   * returns the union of all unbound MetaVars inside the given judgments
+   * @param ignore use e.g. because already bound and should not be captured again
+   */
+  def freeVariables(jdgmnts: Seq[TypingRuleJudgment], ignore: Set[MetaVar] = Set()): Set[MetaVar]
      = jdgmnts.foldLeft(Set.empty[MetaVar])((already, r) => already ++ freeVariables(r, ignore))
   
-  def freeVariables(jdg: TypingRuleJudgment, ignore: Seq[MetaVar]): Set[MetaVar] = jdg match {
+  /**
+   * single TypingRuleJudgment
+   */
+  def freeVariables(jdg: TypingRuleJudgment, ignore: Set[MetaVar]): Set[MetaVar] = jdg match {
     case TypingJudgment(f1, f2, f3)   => freeVariables(f1, ignore) ++ freeVariables(f2, ignore) ++ freeVariables(f3, ignore)
     case TypingJudgmentSimple(f1, f2) => freeVariables(f1, ignore) ++ freeVariables(f2, ignore)
     case FunctionExpJudgment(f)       => freeVariables(f, ignore)
@@ -15,10 +22,13 @@ object FreeVariables {
     case ForallJudgment(alreadyBoundVars, jdgs) => freeVariables(jdgs, ignore ++ alreadyBoundVars)
     case ReduceJudgment(f1, f2) => freeVariables(f1, ignore) ++ freeVariables(f2, ignore)
     case NotJudgment(jdg) => freeVariables(jdg, ignore)
-    case OrJudgment(jdgs) => ???
+    case OrJudgment(jdgs) => freeVariables(jdgs.flatten, ignore)
   }
   
-  def freeVariables(f: FunctionExpMeta, ignore: Seq[MetaVar]): Set[MetaVar] = f match {
+  /**
+   * FunctionExp/FunctionExpMeta
+   */
+  def freeVariables(f: FunctionExpMeta, ignore: Set[MetaVar]): Set[MetaVar] = f match {
     case FunctionMeta(v) if (ignore contains v) => Set()
     case FunctionMeta(v) => Set(v)
     case FunctionExpNot(f) => freeVariables(f, ignore)
@@ -30,8 +40,12 @@ object FreeVariables {
     case FunctionExpIf(cond, f1, f2) => freeVariables(cond, ignore) ++ freeVariables(f1, ignore) ++ freeVariables(f2, ignore)
     case FunctionExpLet(_, f1, f2) => freeVariables(f1, ignore) ++ freeVariables(f2, ignore)
     case FunctionExpApp(_, args@_*) => args.foldLeft(Set.empty[MetaVar])((already, r) => already ++ freeVariables(r, ignore))
+    // NOTE only collecting MetaVars not normal ExpVar
     case FunctionExpVar(_) | FunctionExpTrue | FunctionExpFalse => Set()
   }
 
+  /**
+   * convenience: can use FreeVariables(...) instead of FreeVariables.freeVariables(...)
+   */
   def apply(jdgmnts: Seq[TypingRuleJudgment]) = freeVariables(jdgmnts)
 }
