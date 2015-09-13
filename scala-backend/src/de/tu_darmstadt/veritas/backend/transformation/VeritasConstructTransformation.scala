874dc043b118f44bf8a316aac656165af81c21b7
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-09-13 23:27:05 +0200

    added exception handling to general VeritasConstructTransform

--- scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/VeritasConstructTransformation.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/VeritasConstructTransformation.scala
@@ -19,13 +19,18 @@ trait VeritasConstructTransformation {
    * to a given sequence of VeritasConstructs
    * (recursively traverses the AST behind each VeritasConstruct
    */
-  def apply(vcl: Seq[VeritasConstruct]): Seq[VeritasConstruct] = {   
+  def apply(vcl: Seq[VeritasConstruct]): Seq[VeritasConstruct] = {
     (for (vc <- vcl) yield {
       if (transform.isDefinedAt(vc))
-        apply(transform(vc)) 
+        apply(transform(vc))
       else {
         val newchildren = (vc.children map apply)
-        Seq(vc.transformChildren(newchildren))
+        try {
+          Seq(vc.transformChildren(newchildren))
+        } catch {
+          case c: ClassCastException => throw TransformationError("Child could not be transformed!")
+          case e: Exception          => throw e
+        }
       }
     }).flatten
   }
