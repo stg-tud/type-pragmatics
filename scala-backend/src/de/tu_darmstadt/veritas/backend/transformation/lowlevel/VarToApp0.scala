3fd780e427981bbbb491b1dd1ac6010743ab81bb
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-09-13 23:29:10 +0200

    specified simple transformation from FunctionPatVar/FunctionExpVar to FunctionPatApp/FunctionExpApp

--- scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/lowlevel/VarToApp0.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/lowlevel/VarToApp0.scala
@@ -1,15 +1,20 @@
 package de.tu_darmstadt.veritas.backend.transformation.lowlevel
 
-import de.tu_darmstadt.veritas.backend.veritas.ModuleDef
-import de.tu_darmstadt.veritas.backend.transformation.ModuleDefTransformation
+import de.tu_darmstadt.veritas.backend.veritas.VeritasConstruct
+import de.tu_darmstadt.veritas.backend.veritas.FunctionPatVar
+import de.tu_darmstadt.veritas.backend.veritas.FunctionExpVar
+import de.tu_darmstadt.veritas.backend.veritas.FunctionPatApp
+import de.tu_darmstadt.veritas.backend.veritas.FunctionExpApp
+import de.tu_darmstadt.veritas.backend.transformation.VeritasConstructTransformation
 
 /**
  * no precondition necessary
  * desugars FunctionPatVar and FunctionExpVar to FunctionPatApp/FunctionExpApp with zero arguments
  */
-object VarToApp0 extends ModuleDefTransformation {
-  override protected def apply: PartialFunction[ModuleDef, Seq[ModuleDef]] = {
-    ???
+object VarToApp0 extends VeritasConstructTransformation {
+  override def transform: PartialFunction[VeritasConstruct, Seq[VeritasConstruct]] = {
+    case FunctionPatVar(n) => Seq(FunctionPatApp(n))
+    case FunctionExpVar(n) => Seq(FunctionExpApp(n))
   }
 
 }
