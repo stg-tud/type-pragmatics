13a273070ffd5554a64482194cef2e1392868e6c
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-09-13 23:28:19 +0200

    changed DesugarLemmas from ModuleDefTransformation to VeritasConstructTransformation
    should now transform lemmas recursively through the AST of a module, not just top-level

--- scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/lowlevel/DesugarLemmas.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/lowlevel/DesugarLemmas.scala
@@ -1,19 +1,19 @@
 package de.tu_darmstadt.veritas.backend.transformation.lowlevel
 
-import de.tu_darmstadt.veritas.backend.veritas.ModuleDef
+import de.tu_darmstadt.veritas.backend.veritas.VeritasConstruct
 import de.tu_darmstadt.veritas.backend.veritas.Axioms
 import de.tu_darmstadt.veritas.backend.veritas.Lemmas
 import de.tu_darmstadt.veritas.backend.veritas.Goals
 import de.tu_darmstadt.veritas.backend.veritas.LemmasWithStrategy
 import de.tu_darmstadt.veritas.backend.veritas.GoalsWithStrategy
-import de.tu_darmstadt.veritas.backend.transformation.ModuleDefTransformation
+import de.tu_darmstadt.veritas.backend.transformation.VeritasConstructTransformation
 
 /**
  * no precondition necessary, 
  * simply desugars lemmas to Axioms and Goals
  */
-object DesugarLemmas extends ModuleDefTransformation {
-  override protected def apply: PartialFunction[ModuleDef, Seq[ModuleDef]] = {
+object DesugarLemmas extends VeritasConstructTransformation {
+  override def transform: PartialFunction[VeritasConstruct, Seq[VeritasConstruct]] = {
     case Lemmas(lm, t)                => Seq(Axioms(lm), Goals(lm, t))
     case LemmasWithStrategy(s, lm, t) => Seq(Axioms(lm), GoalsWithStrategy(s, lm, t))
   }
