51277ff0ed6b2199524fd1f2cd0f000ad622b0c4
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-09-15 13:37:03 +0200

    further refined general VeritasConstructTransformation
    introduced precheck method for checking conditions on VeritasConstructs during traversals

--- scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/VeritasConstructTransformation.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/VeritasConstructTransformation.scala
@@ -6,21 +6,35 @@ import de.tu_darmstadt.veritas.backend.veritas.VeritasConstruct
  * abstract trait for transformations of arbitrary VeritasConstructs
  * when implementing, implement the transform method
  * when applying, use apply method
+ * 
+ * optionally, implement precheck if you need to check sth. for each Veritas construct
+ * during a traversal or if you need to collect information about each Veritas construct
+ * during traversal
  */
 trait VeritasConstructTransformation {
   /**
    * translates a Veritas construct of interest into a sequence of other VeritasConstructs
-   * (top-down one pass traversal!)
    */
   def transform: PartialFunction[VeritasConstruct, Seq[VeritasConstruct]]
+  
+  /**
+   * checks for each Veritas construct which is passed during a recursive traversal
+   * whether a certain condition holds - if not, the construct is skipped during the traversal
+   * (can for example be used to skip certain modules in a sequence of modules during a traversal)
+   * 
+   * this method can also be implemented with side-effects, 
+   * in case e.g. some state has to be updated for each Veritas construct traversed
+   */
+  def precheck(vc: VeritasConstruct): Boolean = true
 
   /**
    * recursively apply the transformation defined by transform
    * to a given sequence of VeritasConstructs
-   * (recursively traverses the AST behind each VeritasConstruct
+   * * (top-down one pass traversal!)
+   * (recursively traverses the AST behind each VeritasConstruct)
    */
   def apply(vcl: Seq[VeritasConstruct]): Seq[VeritasConstruct] = {
-    (for (vc <- vcl) yield {
+    (for (vc <- vcl if precheck(vc)) yield {
       if (transform.isDefinedAt(vc))
         apply(transform(vc))
       else {
