97caeeee6c4219fb0da6e51f0558d4a2e5be9003
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-09-15 18:17:59 +0200

    incomplete extension of VarToApp0 transformation
    missing: check whether PatVar/ExpVar corresponds to declared constructors...
    --> not trivial, probably requires restructuring the overall class hierarchy and rethinking the "infrastructure"

--- scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/lowlevel/VarToApp0.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/lowlevel/VarToApp0.scala
@@ -8,13 +8,19 @@ import de.tu_darmstadt.veritas.backend.veritas.FunctionExpApp
 import de.tu_darmstadt.veritas.backend.transformation.VeritasConstructTransformation
 
 /**
- * no precondition necessary
- * desugars FunctionPatVar and FunctionExpVar to FunctionPatApp/FunctionExpApp with zero arguments
+ * desugars FunctionPatVar and FunctionExpVar to FunctionPatApp/FunctionExpApp with zero arguments,
+ * if they are declared constructors/consts
  */
 object VarToApp0 extends VeritasConstructTransformation {
+  var consnames: Seq[String] = Seq() 
+  
   override def transform: PartialFunction[VeritasConstruct, Seq[VeritasConstruct]] = {
     case FunctionPatVar(n) => Seq(FunctionPatApp(n))
     case FunctionExpVar(n) => Seq(FunctionExpApp(n))
+  }
+  
+  override def precheck(vc: VeritasConstruct): Boolean = {
+    ???
   }
 
 }
