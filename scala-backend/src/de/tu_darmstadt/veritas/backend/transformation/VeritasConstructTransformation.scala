1c0d26006d058c0e0ffc8fb723b95e1ddceaf83a
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-09-16 09:57:36 +0200

    implemented more than one traversal strategy for general VeritasConstructTransformations
    to be continued if necessary.....

--- scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/VeritasConstructTransformation.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/VeritasConstructTransformation.scala
@@ -26,6 +26,14 @@ trait VeritasConstructTransformation {
    * in case e.g. some state has to be updated for each Veritas construct traversed
    */
   def precheck(vc: VeritasConstruct): Boolean = true
+  
+  /**
+   * default traversal behavior of the transformation
+   * standard: top-down one pass traversal until first occurrence
+   * 
+   * implement apply, setting 
+   */
+  def apply(vcl: Seq[VeritasConstruct]): Seq[VeritasConstruct] = topDownUntilFirstTraversal(vcl)
 
   /**
    * recursively apply the transformation defined by transform
@@ -33,7 +41,7 @@ trait VeritasConstructTransformation {
    * (top-down one pass traversal, stops traversing as soon as a construct can be transformed via the transform function! -
    * that is, it does not check the children of such a construct again)
    */
-  def apply(vcl: Seq[VeritasConstruct]): Seq[VeritasConstruct] = {
+  final def topDownUntilFirstTraversal(vcl: Seq[VeritasConstruct]): Seq[VeritasConstruct] = {
     (for (vc <- vcl if precheck(vc)) yield {
       if (transform.isDefinedAt(vc))
         transform(vc)
@@ -48,5 +56,31 @@ trait VeritasConstructTransformation {
       }
     }).flatten
   }
+  
+  /**
+   * recursively apply the transformation defined by transform
+   * to a given sequence of VeritasConstructs
+   * (top-down one pass traversal - full traversal also down the children of transformed constructs
+   * careful: do not use for transformations that also return themselves again, as this will yield
+   * an infinite loop)
+   */
+  final def topDownFullTraversal(vcl: Seq[VeritasConstruct]): Seq[VeritasConstruct] = {
+    (for (vc <- vcl if precheck(vc)) yield {
+      if (transform.isDefinedAt(vc))
+        apply(transform(vc))
+      else {
+        val newchildren = (vc.children map apply)
+        try {
+          Seq(vc.transformChildren(newchildren))
+        } catch {
+          case c: ClassCastException => throw TransformationError("Child could not be transformed!")
+          case e: Exception          => throw e
+        }
+      }
+    }).flatten
+  }
+  
+  //TODO: if necessessary, implement more traversal strategies (e.g. bottom-up etc.)
+  
 
 }
