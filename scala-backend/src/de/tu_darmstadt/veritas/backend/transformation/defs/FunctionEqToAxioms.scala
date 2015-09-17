f64dfbf3912deceb4f71fa2cfbb1e4f5a77bcc57
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-09-17 23:55:45 +0200

    further refined FunctionEqToAxioms transformation
    so far just followed the original Stratego implementation

--- scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/defs/FunctionEqToAxioms.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/defs/FunctionEqToAxioms.scala
@@ -11,29 +11,37 @@ import de.tu_darmstadt.veritas.backend.transformation.ModuleTransformation
  * TODO Is it possible to generate a simple precondition with this requirement? Probably not...
  */
 object FunctionEqToAxioms extends ModuleTransformation {
-  //  override def transform: PartialFunction[VeritasConstruct, Seq[VeritasConstruct]] = {
-  //    //generate one block with all the function signatures
-  //    //then a list of axioms from all the function equations
-  //    case Functions(fdefs) =>
-  //      Seq(Functions(fdefs map { case FunctionDef(sig, _) => FunctionDef(sig, Seq()) })) ++
-  //      fdefs flatMap { case FunctionDef(_, feqs) => generateEqAxioms(feqs) }
-  //  }
-  //
-  //  private def generateEqAxioms(feqs: Seq[FunctionEq]): Seq[Axioms] = {
-  //    ???
-  //  }
 
   override def transModuleDefs(mdef: ModuleDef): Seq[ModuleDef] =
-    //note: the trace is not correctly updated here (because this is probably not needed!)
     withSuper(super.transModuleDefs(mdef)) {
       case Functions(fs) =>
         //generate one block with all the function signatures
         //then a list of axioms from all the function equations
-        Seq(Functions(fs map { case FunctionDef(sig, _) => FunctionDef(transFunctionSig(sig), Seq()) })) ++
-          fs flatMap { case FunctionDef(_, feqs) => generateEqAxioms(feqs) }
+        Seq(Functions(fs map { case FunctionDef(sig, _) => FunctionDef(sig, Seq()) })) ++
+          fs flatMap { case FunctionDef(sig, feqs) => functionEqsDatas(feqs, makeArgMetaVars(sig.in), sig.out, Seq()) }
     }
 
-  private def generateEqAxioms(feqs: Seq[FunctionEq]): Seq[Axioms] = {
-    ???
+  private def makeArgMetaVars(args: Seq[SortRef]): Seq[MetaVar] = ???
+
+  private def functionPatProp(fp: FunctionPattern): FunctionExp = ???
+  
+  private def notMatchPrepatsProp(pats: Seq[FunctionPattern], prepats: Seq[FunctionPattern]): FunctionExp = ???
+  
+  private def functionEqData(fe: FunctionEq, argvars: Seq[MetaVar], restype: SortRef, preprop: FunctionExp): Seq[TypingRule] = ???
+
+  private def functionEqsDatas(feqs: Seq[FunctionEq], argvars: Seq[MetaVar], restype: SortRef, prepats: Seq[FunctionPattern]): Seq[Axioms] = {
+    feqs match {
+      case Nil => Nil
+      case (f @ FunctionEq(name, pats, exp)) :: fs => {
+        val patprops = pats map functionPatProp
+        val patbindprops = (argvars.zip(patprops)) map { case (a, p) => FunctionExpEq(FunctionMeta(a), p) }
+        val patprop: FunctionExp = patbindprops.fold(FunctionExpTrue)((e1, e2) => FunctionExpAnd(e1, e2))
+        val notmatchprepatsprop = notMatchPrepatsProp(pats, prepats)
+        val patnotprepatprop = FunctionExpAnd(patprop, notmatchprepatsprop)
+        val functiondata = functionEqData(f, argvars, restype, patnotprepatprop)
+        val restdata = functionEqsDatas(fs, argvars, restype, pats ++ prepats)
+        Seq(Axioms(functiondata)) ++ restdata
+      }
+    }
   }
 }
