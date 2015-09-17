9ac36ffc35ede8d231462a6eed8d3de3c8d94410
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-09-17 20:15:32 +0200

    started adapting FunctionEqToAxioms to new ModuleTransformation

--- scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/defs/FunctionEqToAxioms.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/defs/FunctionEqToAxioms.scala
@@ -6,20 +6,34 @@ import de.tu_darmstadt.veritas.backend.transformation.ModuleTransformation
 /**
  * generates axioms for function equations
  * takes order of function equations into account!
- * --> assumes that FunctionPatVar/FunctionExpVar was already substituted with 
+ * --> assumes that FunctionPatVar/FunctionExpVar was already substituted with
  * FunctionPatApp/FunctionExpApp if there was a clash with constructor names!
  * TODO Is it possible to generate a simple precondition with this requirement? Probably not...
  */
 object FunctionEqToAxioms extends ModuleTransformation {
-//  override def transform: PartialFunction[VeritasConstruct, Seq[VeritasConstruct]] = {
-//    //generate one block with all the function signatures
-//    //then a list of axioms from all the function equations
-//    case Functions(fdefs) =>
-//      Seq(Functions(fdefs map { case FunctionDef(sig, _) => FunctionDef(sig, Seq()) })) ++
-//      fdefs flatMap { case FunctionDef(_, feqs) => generateEqAxioms(feqs) }
-//  }
-//
-//  private def generateEqAxioms(feqs: Seq[FunctionEq]): Seq[Axioms] = {
-//    ???
-//  }
+  //  override def transform: PartialFunction[VeritasConstruct, Seq[VeritasConstruct]] = {
+  //    //generate one block with all the function signatures
+  //    //then a list of axioms from all the function equations
+  //    case Functions(fdefs) =>
+  //      Seq(Functions(fdefs map { case FunctionDef(sig, _) => FunctionDef(sig, Seq()) })) ++
+  //      fdefs flatMap { case FunctionDef(_, feqs) => generateEqAxioms(feqs) }
+  //  }
+  //
+  //  private def generateEqAxioms(feqs: Seq[FunctionEq]): Seq[Axioms] = {
+  //    ???
+  //  }
+
+  override def transModuleDefs(mdef: ModuleDef): Seq[ModuleDef] =
+    //note: the trace is not correctly updated here (because this is probably not needed!)
+    withSuper(super.transModuleDefs(mdef)) {
+      case Functions(fs) =>
+        //generate one block with all the function signatures
+        //then a list of axioms from all the function equations
+        Seq(Functions(fs map { case FunctionDef(sig, _) => FunctionDef(transFunctionSig(sig), Seq()) })) ++
+          fs flatMap { case FunctionDef(_, feqs) => generateEqAxioms(feqs) }
+    }
+
+  private def generateEqAxioms(feqs: Seq[FunctionEq]): Seq[Axioms] = {
+    ???
+  }
 }
