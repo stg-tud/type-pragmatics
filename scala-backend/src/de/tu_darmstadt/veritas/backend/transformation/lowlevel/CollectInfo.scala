f0c67883424987a3e72fe34ee11e02894fecf549
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-09-20 22:59:28 +0200

    traits for collecting sort names and type info

--- scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/lowlevel/CollectInfo.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/lowlevel/CollectInfo.scala
@@ -1,6 +1,7 @@
 package de.tu_darmstadt.veritas.backend.transformation.lowlevel
 
 import de.tu_darmstadt.veritas.backend.veritas._
+import scala.collection.mutable.Map
 import de.tu_darmstadt.veritas.backend.transformation.ModuleTransformation
 
 trait CollectConstructorNames extends ModuleTransformation {
@@ -33,3 +34,70 @@ trait CollectConstructorNames extends ModuleTransformation {
     }
 
 }
+
+trait CollectSortNames extends ModuleTransformation {
+  var sortNames: Seq[String] = Seq()
+
+  override def transModuleDefs(mdef: ModuleDef): Seq[ModuleDef] =
+    //take scoping into account (local blocks and strategy blocks!)
+    //reset consNames to previous value when "leaving" such a block during the traversal
+    withSuper(super.transModuleDefs(mdef)) {
+      case Local(defs) => {
+        val oldCons = sortNames
+        val res = Seq(Local(trace(defs)(transModuleDefs(_))))
+        sortNames = oldCons
+        res
+      }
+      case Strategy(n, i, d) => {
+        val oldCons = sortNames
+        val res = Seq(Strategy(n, trace(i)(transModuleImport(_)), trace(d)(transModuleDefs(_))))
+        sortNames = oldCons
+        res
+      }
+    }
+
+  override def transSortDefs(sd: SortDef): Seq[SortDef] =
+    withSuper(super.transSortDefs(sd)) {
+      case s @ SortDef(_) => {
+        sortNames = sortNames :+ sd.name
+        Seq(s)
+      }
+    }
+
+}
+
+trait CollectTypeInfo extends ModuleTransformation {
+  var constypes: Map[String, (Seq[SortRef], SortRef)] = Map()
+  var functypes: Map[String, (Seq[SortRef], SortRef)] = Map()
+
+  override def transModuleDefs(mdef: ModuleDef): Seq[ModuleDef] =
+    //take scoping into account (local blocks and strategy blocks!)
+    //reset consNames to previous value when "leaving" such a block during the traversal
+    withSuper(super.transModuleDefs(mdef)) {
+      case cons @ Constructors(cdecl) => {
+        (cdecl map { case c @ ConstructorDecl(n, in, out) => constypes.put(n, (in, out)) })
+        Seq(cons)
+      }
+      case funcs @ Functions(fdecl) => {
+        (fdecl map { case c @ FunctionDef(FunctionSig(n, in, out), defs) => functypes.put(n, (in, out)) })
+        Seq(funcs)
+      }
+      case Local(defs) => {
+        val oldconstypes = constypes
+        val oldfunctypes = functypes
+        val res = Seq(Local(trace(defs)(transModuleDefs(_))))
+        constypes = oldconstypes
+        functypes = oldfunctypes
+        res
+      }
+      case Strategy(n, i, d) => {
+        val oldconstypes = constypes
+        val oldfunctypes = functypes
+        val res = Seq(Strategy(n, trace(i)(transModuleImport(_)), trace(d)(transModuleDefs(_))))
+        constypes = oldconstypes
+        functypes = oldfunctypes
+        res
+      }
+    }
+}
+
