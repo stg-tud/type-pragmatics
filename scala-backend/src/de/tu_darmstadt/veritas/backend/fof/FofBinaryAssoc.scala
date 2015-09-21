606eb6e593802cd4de8095a3d8f8818051b13fd1
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-09-22 00:41:56 +0200

    removed old comments

--- scala-backend/src/de/tu_darmstadt/veritas/backend/fof/FofBinaryAssoc.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/fof/FofBinaryAssoc.scala
@@ -31,16 +31,7 @@ object Or {
         case s           => new Or(s)
       }
     }
-  
-  //  def apply(args: Seq[FofUnitary]): Fof = args match {
-  //    case Seq()                         => False
-  //    case Seq(single)                   => single
-  //    case elems if elems.contains(True) => True
-  //    case elems => Or(elems.filterNot {
-  //      case False => true
-  //      case _     => false
-  //    })
-  //  }
+ 
   def unapply(e: Or): Option[Seq[FofUnitary]] = Some(e.args)
 }
 
@@ -62,17 +53,5 @@ object And {
       }
     }
 
-  //    args match {
-  //    case Seq() => True
-  //
-  //    case Seq(single) => single
-  //    case False +: tail => False
-  //    case head +: tail => And(head, tail)
-  //    case elems if elems.contains(False) => False
-  //    case elems => And(elems.filterNot { 
-  //      case True => true
-  //      case _ => false
-  //    })
-  //  }
   def unapply(e: And): Option[Seq[FofUnitary]] = Some(e.args)
 }
