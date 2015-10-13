a9a7bfbd3560edac9e815d6138f264abab5574c6
Daniel Lehmann <daniel.lehmann@stud.tu-darmstadt.de>
2015-10-13 12:02:35 +0200

    added FilterModuleDefs trait and FilterGoalsAndLocals transformation (useful to filter imported Modules)

---
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/FilterModuleDefs.scala
@@ -0,0 +1,28 @@
+package de.tu_darmstadt.veritas.backend.transformation
+
+import de.tu_darmstadt.veritas.backend.veritas.Goals
+import de.tu_darmstadt.veritas.backend.veritas.Local
+import de.tu_darmstadt.veritas.backend.veritas.ModuleDef
+
+trait FilterModuleDefs extends ModuleTransformation {
+  /**
+   * @return true if you want to keep this element, false if it should get removed
+   */
+  /* abstract */ def filterPredicate(x: ModuleDef): Boolean
+  
+  override def transModuleDefs(mdef: ModuleDef): Seq[ModuleDef] = 
+    withSuper(super.transModuleDefs(mdef)) { 
+      case mdef => 
+        if (filterPredicate(mdef))
+          Seq(mdef)
+        else
+          Seq()
+    }
+}
+
+object FilterGoalsAndLocals extends FilterModuleDefs {
+  override def filterPredicate(x: ModuleDef) = x match {
+    case _: Local | _: Goals => false
+    case _ => true
+  }
+}
