d5fa6055a01ea468ff08e66efb67389e3968228c
Daniel Lehmann <daniel.lehmann@stud.tu-darmstadt.de>
2015-10-13 21:14:13 +0200

    fixed handling of Strategy/GoalsWithStrategy in SplitModulesByGoal: "verify-with goals" now only use the axioms at the _end of the definition of strategy blocks_, i.e. no longer axioms _after_ the referenced strategy. Also added Include/Hide/HideAll handling

--- scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/SplitModulesByGoal.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/SplitModulesByGoal.scala
@@ -10,109 +10,161 @@ import de.tu_darmstadt.veritas.backend.veritas.Local
 import de.tu_darmstadt.veritas.backend.veritas.Module
 import de.tu_darmstadt.veritas.backend.veritas.ModuleDef
 import de.tu_darmstadt.veritas.backend.veritas.Strategy
+import de.tu_darmstadt.veritas.backend.veritas.ModuleDefHolder
+import de.tu_darmstadt.veritas.backend.veritas.Axioms
+import de.tu_darmstadt.veritas.backend.veritas.HideAll
+import de.tu_darmstadt.veritas.backend.veritas.Hide
+import de.tu_darmstadt.veritas.backend.veritas.Include
+import de.tu_darmstadt.veritas.backend.veritas.TypingRule
 
 /**
  * Flattens the Module structure (containing recursive ModuleDefs inside local etc. blocks) to 
- * multiple Modules. Hence, after this transformation, no Local/Strategy blocks will be present anymore.
- * Also, no GoalsWithStrategy should be present anymore, they are now desugared to Goals.
+ * multiple Modules. Hence, after this transformation:
+ *  - no Local/Strategy blocks will be present anymore
+ *  - no GoalsWithStrategy should be present anymore, they are now desugared to Goals
+ *  - no Hide/HideAll/Include will be present anymore
  * 
- * Expects Lemmas/LemmasWithStrategy are already desugared.
- * Should basically not change a single-goal Module (except for the name).
- * 
- * Forwards all the imports of the current module into the generated Modules. Hence, imports can
- * be resolved either before (on the "one big Module") or after the splitting (once per each 
- * generated Module).
+ * Expects as input a single Module where
+ *  - Lemmas/LemmasWithStrategy are already desugared (to Axioms+Goals)
+ *  - all Imports are already resolved (it is one self-contained Module without any imports)
  */
-
-// FIXME what to do with Hide/HideAll/Include? Are they desugared/removed after this transformation also?
-
 object SplitModulesByGoal extends ModuleTransformation {
   private var submoduleNameGenerator = new FreshNames(false)
   
   override def transModule(name: String, is: Seq[Import], mdefs: Seq[ModuleDef]): Seq[Module] =
     withSuper(super.transModule(name, is, mdefs)) {
-      case Module(name, imports, mdefs) => {
-        // reinitialize name generator once per Module
+      case mod@Module(name, _, defs) => {
+        // reinitialize name generator whenever this transformation is called on a Module
         submoduleNameGenerator = new FreshNames(false)
-        splitRecursiveModuleDefs(mdefs, name, imports)
+        
+        splitRecursive(mod, name, Seq(), Map(), Seq())._2
       }
     }
 
-  private def splitRecursiveModuleDefs(mdefs: Seq[ModuleDef], moduleNamePrefix: String, imports: Seq[Import]): Seq[Module] = {
+  /**
+   * @return a pair (active ModuleDefs at the end if the processing, generated Submodules)
+   */
+  private def splitRecursive(holder: ModuleDefHolder, 
+                             moduleNamePrefix: String, 
+                             _allDefs: Seq[ModuleDef],
+                             _strategiesWithActiveDefs: Map[Strategy, Seq[ModuleDef]],
+                             _activeDefs: Seq[ModuleDef] 
+                             ): (Seq[ModuleDef], Seq[Module]) = {
+    if (!holder.imports.isEmpty)
+      throw new TransformationError("Imports must be processed (== removed and their elements included in the ModuleDefs) before this transformation, got: " + holder)
+
     var generatedSubmodules = List.empty[Module]
 
-    // iterate from front to back, also remember all mdefs seen UP TO CURRENT:
-    var mdefsUptoCurrent = List.empty[ModuleDef]
-    mdefs foreach {
+    // iterate from front to back, keep track of: 
+    // (all, active or not) ModuleDefs
+    var allDefs = _allDefs    
+    // Strategies (and the Defs at the end of their definition)
+    var strategiesWithActiveDefs = _strategiesWithActiveDefs
+    // "active" - that is visible in the resulting (Sub-)Module - Defs
+    var activeDefs = _activeDefs
+  
+    holder.defs foreach {
 
       /*
        * a) whenever a recursive ModuleDef "holder" is seen ("local" or "strategy") -> recursive call, append the resulting generated Modules
        */
-      case Local(recMdefs) => {
+      case local@Local(recMdefs) => {
         val recModuleNamePrefix = moduleNamePrefix + "-local"
-        generatedSubmodules ++= splitRecursiveModuleDefs(mdefsUptoCurrent ++ recMdefs, recModuleNamePrefix, imports)
+        generatedSubmodules ++= splitRecursive(local, recModuleNamePrefix, allDefs, strategiesWithActiveDefs, activeDefs)._2
       }
-      case Strategy(stratName, stratImports, recMdefs) => {
+      case strat@Strategy(stratName, _, recMdefs) => {
         val recModuleNamePrefix = moduleNamePrefix + "-" + stratName
-        val recImports = imports ++ stratImports
-        generatedSubmodules ++= splitRecursiveModuleDefs(mdefsUptoCurrent ++ recMdefs, recModuleNamePrefix, recImports)
+        val (processedStrategyDefs, newGeneratedSubmodules) = splitRecursive(strat, recModuleNamePrefix, allDefs, strategiesWithActiveDefs, activeDefs)
+        generatedSubmodules ++= newGeneratedSubmodules
+        // remember the strategy and the active axioms at the end its definition site (see GoalsWithStrategy)
+        strategiesWithActiveDefs += (strat -> (activeDefs ++ processedStrategyDefs).distinct)
+      }
+
+      /*
+       * b) HideAll/Hide/Include influence the active axiom set 
+       */
+      case Hide(ruleNames) => activeDefs = removeAxiomsWithNames(activeDefs, ruleNames)
+      case HideAll => activeDefs = activeDefs filter {
+        case _: Axioms => false
+        case _ => true
+      }
+      case inc@Include(axiomNames) => {
+        val referencedAxioms = for (axiomName <- axiomNames) yield {
+          if (!findAxiomByName(activeDefs, axiomName).isEmpty)
+            throw TransformationError("Referenced axiom \"" + axiomName + "\" in include " + inc + " is already active, include is unnecessary.")
+          
+          val foundAxioms = findAxiomByName(allDefs, axiomName)
+          foundAxioms match {
+            case Seq() => throw TransformationError("Referenced axiom \"" + axiomName + "\" in include " + inc + " was not found in: " + allDefs)
+            case Seq(rule) => rule
+            case _ => throw TransformationError("Referenced axiom \"" + axiomName + "\" in include " + inc + " was found multiple times in: " + allDefs)
+          }
+        }
+        activeDefs +:= Axioms(referencedAxioms) 
       }
       
       /*
        * b) whenever a Goal is seen -> add new Module to output Seq
        */
-      case Goals(goals, timeout) => goals map { goal =>
+      case Goals(goals, timeout) => goals foreach { goal =>
         generatedSubmodules :+= Module(
             submoduleNameGenerator.freshName(moduleNamePrefix + "-" + goal.name), 
-            imports, 
-            mdefsUptoCurrent :+ Goals(Seq(goal), timeout)
+            Seq(), 
+            activeDefs :+ Goals(Seq(goal), timeout)
         )
       }
       // desugar to goal + referenced strategy's ModuleDefs
       case GoalsWithStrategy(strategyName, goals, timeout) => {
         // search for the referenced strategy, include its elements before the goal
-        val referencedStrategy = findStrategyByName(mdefs, strategyName)
-        val referencedStrategyStripped = stripGoalsAndLocals(referencedStrategy.defs)
+        // NOTE this only searches top-level strategies, but since strategies inside a local cannot
+        // be referenced anyway, it is correct.
+        val referencedStrategy = strategiesWithActiveDefs.keys.find { 
+          case Strategy(n, _, _) if n == strategyName => true
+          case _ => false
+        }.getOrElse(throw TransformationError("GoalWithStrategy referenced strategy \"" + strategyName + "\", but couldn't find it in the Modules defs seen so far: " + allDefs))
         
-        goals map { goal => 
+        goals foreach { goal => 
           generatedSubmodules :+= Module(
               submoduleNameGenerator.freshName(moduleNamePrefix + "-" + goal.name), 
-              // also add the strategy imports to the generated submodules
-              imports ++ referencedStrategy.imports, 
-              mdefsUptoCurrent ++ referencedStrategyStripped :+ Goals(Seq(goal), timeout)
+              Seq(), 
+              strategiesWithActiveDefs(referencedStrategy) :+ Goals(Seq(goal), timeout)
           )        
         }
       }
-    
+
       /*
        * c) Lemmas should already be desugared -> ERROR
        */
       case e: Lemmas => throw TransformationError("splitting of module failed, expected Lemmas to already be desugared but got: " + e) 
       case e: LemmasWithStrategy => throw TransformationError("splitting of module failed, expected Lemmas to already be desugared but got: " + e) 
-            
+
       /*
        * d) all others -> pass through
        */
-      case e => mdefsUptoCurrent :+= e
+      case e => {
+        allDefs :+= e
+        activeDefs :+= e
+      }
     }
     
-    generatedSubmodules
+    (activeDefs, generatedSubmodules)
   }
 
-  // FIXME this only searches top-level strategies, correct?
-  private def findStrategyByName(haystack: Seq[ModuleDef], strategyName: String): Strategy = 
-    haystack.collectFirst{
-      case s @ Strategy(n, _, _) if n == strategyName => s
-    }.getOrElse(
-      throw TransformationError("splitting of module failed, a GoalWithStrategy referenced strategy \"" + strategyName + "\", but couldn't find it in the Modules body"))
+  private def findAxiomByName(haystack: Seq[ModuleDef], axiomName: String): Seq[TypingRule] = haystack flatMap {
+    case holder: ModuleDefHolder => findAxiomByName(holder.defs, axiomName)
+    case Axioms(typingRules) => typingRules.filter(_.name == axiomName)
+    case _ => Seq()
+  }
 
-  private def stripGoalsAndLocals(mdefs: Seq[ModuleDef]): Seq[ModuleDef] = mdefs flatMap {
-      // FIXME correct/desired to remove all locals?
-      case _ : Local => None
-      // recursively strip
-      case Strategy(name, imports, defs) => Some(Strategy(name, imports, stripGoalsAndLocals(defs)))
-      case _ : Goals => None
-      case _ : GoalsWithStrategy => None
-      case e => Some(e)
+  private def removeAxiomsWithNames(input: Seq[ModuleDef], names: Seq[String]): Seq[ModuleDef] = input flatMap {
+    // all of these are _removed_ as part of this transformation, shouldn't be present in the first place
+    case _: Hide | HideAll | _: Include | _: Local | _: Strategy | _: Goals | _: GoalsWithStrategy | _: Lemmas | _: LemmasWithStrategy => throw TransformationError("Module defs when filtering for axioms should not contain Hide/HideAll/Include/Local/Strategy/Goals(WithStrategy)/Lemmas(WithStrategy))")
+
+    case Axioms(typingRules) => Some(Axioms(typingRules filter {
+      case TypingRule(ruleName, _, _) if names contains ruleName => false
+      case _ => true
+    }))
+    
+    case t => Some(t)
   }
 }
