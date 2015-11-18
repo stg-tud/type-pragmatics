eae01e5d3f03dc6f482e0055347d91541601afdd
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-11-18 20:43:33 +0100

    fixed a bug that removed too many substitution equations
    restructured module transformations such that the inlining of subformulas and the removal of the inlined premises are two separate transformations that have to be executed sequentially; the default implementation of the transformation for removing only removes premises that were actually used for inlining!

--- scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/defs/CollectInlineEquations.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/defs/CollectInlineEquations.scala
@@ -151,6 +151,19 @@ trait CollectInlineEquations extends ModuleTransformation {
 }
 
 trait InlineSubformulas extends ModuleTransformation with CollectInlineEquations {
+  /**
+   * save which variables were used for substitution in a single typing rule
+   *
+   */
+  var substitutedVars: Set[String] = Set()
+
+  override def transTypingRules(tr: TypingRule): Seq[TypingRule] = {
+    //reset state for each typing rule that is traversed
+    substitutedVars = Set()
+    tr match {
+      case t @ TypingRule(n, prems, conss) => super.transTypingRules(t)
+    }
+  }
 
   /**
    * override to control which constructs actually get substituted
@@ -159,6 +172,7 @@ trait InlineSubformulas extends ModuleTransformation with CollectInlineEquations
    *
    * can use information from path variable, for example
    * parameter vc is the construct itself
+   *
    */
   def checkSubstitute(vc: VeritasConstruct): Boolean = {
     val relparent = if (vc.isInstanceOf[MetaVar]) path(2) else path(1)
@@ -168,15 +182,83 @@ trait InlineSubformulas extends ModuleTransformation with CollectInlineEquations
     }
   }
 
+  // inside ors, apply substitution that is given by orImplSubstitutions!
+  // this ensures that substitution within Ors that represent implications works as desired
+  override def transTypingRuleJudgment(trj: TypingRuleJudgment): TypingRuleJudgment =
+    withSuper(super.transTypingRuleJudgment(trj)) {
+      case OrJudgment(orc) => {
+        val oldchosen = chosenSubstitutions
+        chosenSubstitutions = orImplSubstitutions
+        val substitutedCases = orc map (s => s flatMap transTypingRuleJudgments)
+        chosenSubstitutions = oldchosen
+        OrJudgment(substitutedCases)
+      }
+    }
+
+  // inside ors, apply substitution that is given by orImplSubstitutions!
+  // this ensures that substitution within Ors that represent implications works as desired
+  override def transTypingRuleJudgments(trj: TypingRuleJudgment): Seq[TypingRuleJudgment] =
+    withSuper(super.transTypingRuleJudgments(trj)) {
+      case OrJudgment(orc) => {
+        val oldchosen = chosenSubstitutions
+        chosenSubstitutions = orImplSubstitutions
+        val substitutedCases = orc map (s => s flatMap transTypingRuleJudgments)
+        chosenSubstitutions = oldchosen
+        Seq(OrJudgment(substitutedCases))
+      }
+    }
+
+  //substitute meta vars with meta vars in quantified varlists
+  //add variables that were used to substitutedVars
+  override def transMetaVar(m: MetaVar): MetaVar =
+    if (chosenSubstitutions.isDefinedAt(m) && checkSubstitute(m))
+      chosenSubstitutions(m) match {
+        case FunctionMeta(n) => { substitutedVars = substitutedVars + m.name; n }
+        case _               => m
+      }
+    else m
+
+  override def transMetaVars(m: MetaVar): Seq[MetaVar] =
+    if (chosenSubstitutions.isDefinedAt(m) && checkSubstitute(m))
+      chosenSubstitutions(m) match {
+        case FunctionMeta(n) => { substitutedVars = substitutedVars + m.name; Seq(n) }
+        case _               => Seq(m)
+      }
+    else Seq(m)
+
+  //in FunctionExpMeta, substitute meta vars with what is given in chosenSubstitution
+  //add variables that were used to substitutedVars
+  override def transFunctionExpMeta(f: FunctionExpMeta): FunctionExpMeta =
+    withSuper(super.transFunctionExpMeta(f)) {
+      case fm @ FunctionMeta(m) if (checkSubstitute(fm) && chosenSubstitutions.isDefinedAt(m)) =>
+        { substitutedVars = substitutedVars + m.name; transFunctionExpMeta(chosenSubstitutions(m)) } //substitute in substitution, if necessary
+    }
+
+  override def transFunctionExpMetas(f: FunctionExpMeta): Seq[FunctionExpMeta] =
+    withSuper(super.transFunctionExpMetas(f)) {
+      case fm @ FunctionMeta(m) if (checkSubstitute(fm) && chosenSubstitutions.isDefinedAt(m)) =>
+        { substitutedVars = substitutedVars + m.name; transFunctionExpMetas(chosenSubstitutions(m)) }
+    }
+
+}
+
+/**
+ * trait to control which equations used for substitution
+ * will be removed from the a typing rule
+ *
+ * takes as parameter a set with variables that were substituted during a previous transformation
+ */
+class RemoveUnusedPremises(substitutedVars: Set[String]) extends ModuleTransformation {
   /**
    * override to control which of the equations marked for inlining
    * get actually removed from the premises
    *
-   * default: all that are in substitution map (also the ones that are not used!)
+   * default: remove all premises that were used at least once during substitution
+   * (but leave premises that were never used!)
    */
   def removeNamingPremise(feq: FunctionExpEq): Boolean =
     feq match {
-      case FunctionExpEq(FunctionMeta(m), r) if (chosenSubstitutions.keySet contains m) => true
+      case FunctionExpEq(FunctionMeta(m), r) if (substitutedVars contains m.name) => true
       case _ => false
     }
 
@@ -208,7 +290,7 @@ trait InlineSubformulas extends ModuleTransformation with CollectInlineEquations
           Seq(ForallJudgment(vl filter (allFV contains _), jl))
         }
       }
-      case OrJudgment(orc) => substituteOrImpl(orc)
+      case OrJudgment(orc) => removeFromOrImpl(orc)
       case n @ NotJudgment(j) => {
         j match {
           case f @ FunctionExpJudgment(eq @ FunctionExpEq(_, _)) if (removeNamingPremise(eq)) => Seq()
@@ -217,23 +299,20 @@ trait InlineSubformulas extends ModuleTransformation with CollectInlineEquations
       }
     }
 
-  private def substituteOrImpl(ors: Seq[Seq[TypingRuleJudgment]]): Seq[TypingRuleJudgment] = {
+  private def removeFromOrImpl(ors: Seq[Seq[TypingRuleJudgment]]): Seq[TypingRuleJudgment] = {
     val concs = ors filterNot (s => (s.size == 1) && s.head.isInstanceOf[NotJudgment])
     if (concs.isEmpty)
       Seq(OrJudgment(ors))
     else {
-      val oldchosen = chosenSubstitutions
-      chosenSubstitutions = orImplSubstitutions
       val prems = ors filter (s => (s.size == 1) && s.head.isInstanceOf[NotJudgment])
       //remove implication premises that are marked as substitution equations
       val newprems = prems filterNot {
         case Seq(NotJudgment(FunctionExpJudgment(eq @ FunctionExpEq(_, _)))) if (removeNamingPremise(eq)) => true
         case _ => false
       }
-      //to the rest, apply the substitution given by orImplSubstitutions
 
+      // traverse remaining cases
       val restors = (newprems ++ concs) map (s => s flatMap transTypingRuleJudgments)
-      chosenSubstitutions = oldchosen
       if (restors.size == 0)
         Seq()
       else if (restors.size == 1)
@@ -242,37 +321,9 @@ trait InlineSubformulas extends ModuleTransformation with CollectInlineEquations
     }
   }
 
-  // the following implements the actual substitution
-
-  //substitute meta vars with meta vars in quantified varlists
-  override def transMetaVar(m: MetaVar): MetaVar =
-    if (chosenSubstitutions.isDefinedAt(m) && checkSubstitute(m))
-      chosenSubstitutions(m) match {
-        case FunctionMeta(n) => n
-        case _               => m
-      }
-    else m
-
-  override def transMetaVars(m: MetaVar): Seq[MetaVar] =
-    if (chosenSubstitutions.isDefinedAt(m) && checkSubstitute(m))
-      chosenSubstitutions(m) match {
-        case FunctionMeta(n) => Seq(n)
-        case _               => Seq(m)
-      }
-    else Seq(m)
-
-  override def transFunctionExpMeta(f: FunctionExpMeta): FunctionExpMeta =
-    withSuper(super.transFunctionExpMeta(f)) {
-      case fm @ FunctionMeta(m) if (checkSubstitute(fm) && chosenSubstitutions.isDefinedAt(m)) => transFunctionExpMeta(chosenSubstitutions(m)) //substitute in substitution, if necessary
-    }
-
-  override def transFunctionExpMetas(f: FunctionExpMeta): Seq[FunctionExpMeta] =
-    withSuper(super.transFunctionExpMetas(f)) {
-      case fm @ FunctionMeta(m) if (checkSubstitute(fm) && chosenSubstitutions.isDefinedAt(m)) => transFunctionExpMetas(chosenSubstitutions(m))
-    }
-
 }
 
+//does not remove equality premises used for substitution!
 object InlineEverythingOnce extends InlineSubformulas
 
 //fixpoint iteration of InlineEverythingOnce
@@ -283,3 +334,24 @@ object InlineEverythingFP extends ModuleTransformation {
     else newmd
   }
 }
+
+//first apply InlineEverythingOnce
+//at the end remove the premises that were used for substitution
+//from all implications (aka typing rules)
+object InlineEverythingAndRemomePrems extends ModuleTransformation {
+  override def transTypingRules(tr: TypingRule): Seq[TypingRule] = {
+    val newtr = InlineEverythingOnce.transTypingRules(tr)
+    val RemPremInst = new RemoveUnusedPremises(InlineEverythingOnce.substitutedVars)
+    newtr flatMap RemPremInst.transTypingRules
+  }
+}
+
+//fixpoint iteration of InlineEverythingAndRemomePrems
+object InlineEverythingAndRemomePremsFP extends ModuleTransformation {
+  override def apply(m: Seq[Module]): Seq[Module] = {
+    val newmd = InlineEverythingAndRemomePrems(m)
+    if (newmd != m) apply(newmd)
+    else newmd
+  }
+}
+
