45f7c6870005fcd51babef4b8b93afbdfc792417
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-11-23 11:22:09 +0100

    corrected consistency check generation: goal does not become part of axiom set!

--- scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/SetupConsistencyCheck.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/SetupConsistencyCheck.scala
@@ -10,8 +10,8 @@ object SetupConsistencyCheck extends ModuleTransformation {
 
   override def transModuleDefs(mdef: ModuleDef): Seq[ModuleDef] =
     withSuper(super.transModuleDefs(mdef)) {
-      case Goals(gs, t) => Seq(Axioms(trace(gs)(transTypingRules(_))),
-        Goals(Seq(TypingRule("false-goal", Seq(), Seq(FunctionExpJudgment(FunctionExpFalse)))), None))
+      // throw the actual goal away for a consistency check - shall only check axioms!
+      case Goals(gs, t) => Seq(Goals(Seq(TypingRule("false-goal", Seq(), Seq(FunctionExpJudgment(FunctionExpFalse)))), None))
     }
 
 }
