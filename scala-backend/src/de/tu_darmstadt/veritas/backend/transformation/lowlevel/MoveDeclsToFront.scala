6ce35eac859b6d66232f15a6fba6e3e57e5d11bb
Sebastian Erdweg <seba.github@gmail.com>
2015-11-26 11:40:47 +0100

    fix buggy move decls transformation

--- scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/lowlevel/MoveDeclsToFront.scala
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/lowlevel/MoveDeclsToFront.scala
@@ -5,6 +5,7 @@ import de.tu_darmstadt.veritas.backend.veritas.function._
 import de.tu_darmstadt.veritas.backend.transformation.ModuleTransformation
 import de.tu_darmstadt.veritas.backend.transformation.TransformationError
 import de.tu_darmstadt.veritas.backend.Configuration
+import scala.util.matching.Regex
 
 /**
  * moves declarations of sorts, constructors, functions etc. to the front of the module 
@@ -18,19 +19,16 @@ object MoveDeclsToFront extends ModuleTransformation {
   var funcdecls: Seq[Functions] = Seq()
   var pfuncdecls: Seq[PartialFunctions] = Seq()
 
-  override def apply(m: Seq[Module])(implicit config: Configuration): Seq[Module] = {
-    //make sure mutable state is initialized upon application
+  override def transModule(name: String, is: Seq[Import], mdefs: Seq[ModuleDef]): Seq[Module] = {
     sortdecls = Seq()
     constdecls = Seq()
+    datatypes = Seq()
     funcdecls = Seq()
     pfuncdecls = Seq()
-    super.apply(m)
-  }
-
-  override def transModule(name: String, is: Seq[Import], mdefs: Seq[ModuleDef]): Seq[Module] = {
+    
     val newimps = trace(is)(transModuleImport(_))
-    val oldmdefs = trace(mdefs)(transModuleDefs(_))
-    val newmdefs = (sortdecls ++ datatypes ++ constdecls ++ funcdecls ++ pfuncdecls).distinct ++ oldmdefs
+    val otherdefs = trace(mdefs)(transModuleDefs(_))
+    val newmdefs = sortdecls ++ datatypes ++ constdecls ++ funcdecls ++ pfuncdecls ++ otherdefs
 
     Seq(Module(name, newimps, newmdefs))
   }
@@ -39,9 +37,7 @@ object MoveDeclsToFront extends ModuleTransformation {
     withSuper(super.transModuleDefs(mdef)) {
       case s@Sorts(_)             => { sortdecls :+= s; Seq() }
       case cs@Consts(_,_)          => { constdecls :+= cs; Seq() }
-      case dt@DataType(_,_,_)   => { 
-        datatypes :+= dt; Seq() 
-      }
+      case dt@DataType(_,_,_)   => { datatypes :+= dt; Seq() }
       case Functions(fs)        => { funcdecls :+= Functions(fs map (f => FunctionDef(f.signature, Seq()))); Seq() }
       case PartialFunctions(fs) => { pfuncdecls :+= PartialFunctions(fs map (f => FunctionDef(f.signature, Seq()))); Seq() }
       case Local(_) => throw TransformationError("Local-block not expected in transformation MoveDeclsToFront!")
