2185429e66d16859c302153fe5d42fa04a23198f
Michael Raulf <jokest@web.de>
2015-12-03 20:50:45 +0100

    Added rudimentary support for princess prover.

--- benchmarking/src/main/scala/veritas/benchmarking/ProverConfig.scala
+++ benchmarking/src/main/scala/veritas/benchmarking/ProverConfig.scala
@@ -4,6 +4,7 @@ import java.io.File
 
 import scopt.OptionParser
 import veritas.benchmarking.vampire.{VampireTraceAnalisisOptions, VampireTraceAnalisis, VampireConfig}
+import veritas.benchmarking.princess.PrincessConfig
 
 import scala.sys.process.ProcessLogger
 
@@ -49,10 +50,17 @@ trait ProverConfig {
   val proverCommand: File
   val acceptedFileFormats: Set[String]
 
-
+  // todo: automatically append .exe under windows systems
   def findBinaryInPath(command: String): File = {
     for (p <- System.getenv("PATH").split(File.pathSeparator);
          f = new File(p, command) if f.exists() && f.canExecute)
+      return f
+    null
+  }
+
+  def findFileInPath(command: String) : File = {
+    for (p <- System.getenv("PATH").split(File.pathSeparator);
+         f = new File(p, command) if f.exists())
       return f
     null
   }
@@ -79,6 +87,9 @@ object ProverConfig {
     _configs += c.name -> c
     _configs += c_sat.name -> c_sat
   }
+
+  _configs += "princess" -> PrincessConfig()
+
   _contributedOptions = _contributedOptions :+ VampireTraceAnalisisOptions
 
   _configs filter (kv => if (kv._2.isValid) true else {println(s"** Removing invalid configuration ${kv._1}"); false})
---
+++ benchmarking/src/main/scala/veritas/benchmarking/princess/PrincessConfig.scala
@@ -0,0 +1,94 @@
+package veritas.benchmarking.princess
+
+import java.io.File
+import java.util.regex.Pattern
+
+import veritas.benchmarking._
+
+case class PrincessConfig()
+    extends ProverConfig {
+
+  def isValid = proverCommand != null
+
+  override val name = s"princess"
+  override val proverCommand = findBinaryInPath(s"java")
+
+  override val acceptedFileFormats = Set(".fof", ".tff")
+
+  def makeCall(file: File, timeout: Int, fullLogs: Boolean) = {
+    var call = Seq(proverCommand.getAbsolutePath)
+    call = call ++ Seq("-Xss20000k", "-Xmx1500m", "-noverify", "ap.CmdlMain", "-inputFormat=tptp")
+    if (timeout > 0)
+      //java -Xss20000k -Xmx1500m -noverify ap.CmdlMain -printTree -inputFormat=tptp -timeout=%1 %2
+      call = call :+ ("-timeout=" + timeout.toString)
+
+
+    if (fullLogs) {
+      call = call :+ "+printTree" // doesn't seem to work.
+    }
+    call = call :+ file.getAbsolutePath
+    println(call)
+    call
+  }
+
+  def tryExtractTimeSeconds(output: String) = {
+    if(output.length > 20)
+      None
+    else {
+      println("extracting time")
+      val end = output.lastIndexOf("ms")
+      val start = output.lastIndexOf("\n", end) + 1
+      if (start < 0 || end < 0)
+        None
+      else {
+        val s = output.substring(start, end)
+        val d = s.toDouble / 1000
+        println(d)
+        Some(d)
+      }
+    }
+  }
+
+  override def newResultProcessor(file: File, timeout: Int) = PrincessResultProcessor(file, timeout)
+
+  case class PrincessResultProcessor(file: File, timeout: Int) extends ResultProcessor {
+
+    var status: ProverStatus = _
+    var time: Option[Double] = _
+
+    var proofBuilder: StringBuilder = _
+
+    override def out(s: => String) = try {
+      //println(s)
+      if (s.contains("% SZS status TimeOut"))
+        status = Inconclusive("Timeout")
+      else if (s.contains("% SZS status Theorem")) {
+        status = Proved
+      }
+    } catch {
+      case e: Exception => println(s"Error ${e.getMessage} in $s")
+        throw e
+    }
+    
+    override def buffer[T](f: => T) = f // no setup or teardown
+    override def err(s: => String) = try {
+        println(s)
+      if (s.contains("ms")) {
+        time = tryExtractTimeSeconds(s)
+      }
+    } catch {
+      case e: Exception => println(s"Error ${e.getMessage} in $s")
+        throw e
+    }
+
+    override def result =
+      if (status == null)
+        new ProverResult(Inconclusive("Unknown"), time, StringDetails("Inconclusive"))
+      else status match {
+        case Proved => new ProverResult(Proved, time, StringDetails(""))
+        case Disproved => new ProverResult(Disproved, time, StringDetails("Disproved"))
+        case Inconclusive(reason) => new ProverResult(Inconclusive(reason), time, StringDetails("Inconclusive"))
+      }
+  }
+}
+
