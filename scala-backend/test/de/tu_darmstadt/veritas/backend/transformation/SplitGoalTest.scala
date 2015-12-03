e355c901a5a50a7252401689584fb6fee68fc16d
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-12-04 00:10:35 +0100

    added another test for import resolving
    TODO: put import resolving tests in a separate file + fix duplication bug

--- scala-backend/test/de/tu_darmstadt/veritas/backend/transformation/SplitGoalTest.scala
+++ scala-backend/test/de/tu_darmstadt/veritas/backend/transformation/SplitGoalTest.scala
@@ -4,11 +4,20 @@ import org.scalatest.FunSuite
 import de.tu_darmstadt.veritas.backend.veritas._
 import de.tu_darmstadt.veritas.backend.Backend
 import de.tu_darmstadt.veritas.backend.veritas.function.FunctionExpTrue
+import de.tu_darmstadt.veritas.backend.transformation.imports.ResolveImports
 
 class SplitGoalTest extends FunSuite {
 
-  def genTrueRule(name: String) = TypingRule("test1", Seq(), Seq(FunctionExpJudgment(FunctionExpTrue)))
+  def genTrueRule(name: String) = TypingRule(name, Seq(), Seq(FunctionExpJudgment(FunctionExpTrue)))
 
+  test("No goal duplication during import resolve") {
+    val mod = Module("test", Seq(), Seq(Goals(Seq(genTrueRule("test")), None)))
+
+    val res = ResolveImports(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(res.head.defs.length == 1)
+  }
+  
   test("No goal duplication - single goal") {
     val mod = Module("test", Seq(), Seq(Goals(Seq(genTrueRule("test")), None)))
 
