1d992dd799a279ab5d99d0dc126dff87443bd64f
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2015-12-04 18:03:12 +0100

    some tests for import resolution
    one test currently failing, TODO!

--- scala-backend/test/de/tu_darmstadt/veritas/backend/transformation/ImportResolutionTest.scala
+++ scala-backend/test/de/tu_darmstadt/veritas/backend/transformation/ImportResolutionTest.scala
@@ -8,10 +8,23 @@ import de.tu_darmstadt.veritas.backend.Backend
 import de.tu_darmstadt.veritas.backend.veritas.Goals
 import org.scalatest.FunSuite
 import de.tu_darmstadt.veritas.backend.veritas.Module
+import de.tu_darmstadt.veritas.backend.veritas.function.FunctionSig
+import de.tu_darmstadt.veritas.backend.veritas.SortRef
+import de.tu_darmstadt.veritas.backend.veritas.MetaVar
+import de.tu_darmstadt.veritas.backend.veritas.function.FunctionExpApp
+import de.tu_darmstadt.veritas.backend.veritas.function.FunctionMeta
+import de.tu_darmstadt.veritas.backend.veritas.function.FunctionDef
+import de.tu_darmstadt.veritas.backend.veritas.Resolved
+import de.tu_darmstadt.veritas.backend.veritas.ModuleDef
+import de.tu_darmstadt.veritas.backend.veritas.Functions
 
 class ImportResolutionTest extends FunSuite {
 
   def genTrueRule(name: String) = TypingRule(name, Seq(), Seq(FunctionExpJudgment(FunctionExpTrue)))
+
+  def genTestFunctionSig(name: String) = Functions(Seq(FunctionDef(FunctionSig(name, Seq(SortRef("A")), SortRef("B")), Seq())))
+
+  def genFunctionApp(name: String, mv: MetaVar) = FunctionExpApp(name, Seq(FunctionMeta(mv)))
 
   test("No goal duplication during import resolve") {
     val mod = Module("test", Seq(), Seq(Goals(Seq(genTrueRule("test")), None)))
@@ -20,5 +33,111 @@ class ImportResolutionTest extends FunSuite {
 
     assert(res.head.defs.length == 1)
   }
+
+  test("Correct simple Import") {
+    val impdef = genTestFunctionSig("f")
+    val imp = Resolved(Module("ImportMe", Seq(), Seq(impdef)))
+
+    val mod = Module("test", Seq(imp), Seq(Goals(Seq(genTrueRule("test")), None)))
+
+    val resmod = Module("test", Seq(), Seq(impdef, Goals(Seq(genTrueRule("test")), None)))
+
+    val res = ResolveImports(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(res.head == resmod)
+
+  }
+
+  test("No goal Import") {
+    val impdef = genTestFunctionSig("f")
+    val imp = Resolved(Module("ImportMe", Seq(), Seq(impdef, Goals(Seq(genTrueRule("donotimportme")), None))))
+
+    val mod = Module("test", Seq(imp), Seq(Goals(Seq(genTrueRule("test")), None)))
+
+    val resmod = Module("test", Seq(), Seq(impdef, Goals(Seq(genTrueRule("test")), None)))
+
+    val res = ResolveImports(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(res.head == resmod)
+
+  }
+
+  test("Correct multiple simple Import (does not modify imp-order)") {
+    val impdef1 = genTestFunctionSig("f1")
+    val imp1 = Resolved(Module("ImportMe1", Seq(), Seq(impdef1)))
+
+    val impdef2 = genTestFunctionSig("f2")
+    val imp2 = Resolved(Module("ImportMe2", Seq(), Seq(impdef2)))
+
+    val impdef3 = genTestFunctionSig("f3")
+    val imp3 = Resolved(Module("ImportMe3", Seq(), Seq(impdef3)))
+
+    val mod = Module("test", Seq(imp1, imp2, imp3), Seq(Goals(Seq(genTrueRule("test")), None)))
+
+    val resmod = Module("test", Seq(), Seq(impdef1, impdef2, impdef3, Goals(Seq(genTrueRule("test")), None)))
+
+    val res = ResolveImports(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(res.head == resmod)
+  }
   
+  test("Correct Import simple dependencies") {
+    val impdef1 = genTestFunctionSig("f1")
+    val imp1 = Resolved(Module("ImportMe1", Seq(), Seq(impdef1)))
+
+    val impdef2 = genTestFunctionSig("f2")
+    val imp2 = Resolved(Module("ImportMe2", Seq(imp1), Seq(impdef2)))
+
+    val impdef3 = genTestFunctionSig("f3")
+    val imp3 = Resolved(Module("ImportMe3", Seq(imp2), Seq(impdef3)))
+
+    val mod = Module("test", Seq(imp3), Seq(Goals(Seq(genTrueRule("test")), None)))
+
+    val resmod = Module("test", Seq(), Seq(impdef1, impdef2, impdef3, Goals(Seq(genTrueRule("test")), None)))
+
+    val res = ResolveImports(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(res.head == resmod)
+  }
+  
+  test("Correct Import simple dependencies - no module duplication") {
+    val impdef1 = genTestFunctionSig("f1")
+    val imp1 = Resolved(Module("ImportMe1", Seq(), Seq(impdef1)))
+
+    val impdef2 = genTestFunctionSig("f2")
+    val imp2 = Resolved(Module("ImportMe2", Seq(imp1), Seq(impdef2)))
+
+    val impdef3 = genTestFunctionSig("f3")
+    val imp3 = Resolved(Module("ImportMe3", Seq(imp1, imp2), Seq(impdef3)))
+
+    val mod = Module("test", Seq(imp3), Seq(Goals(Seq(genTrueRule("test")), None)))
+
+    val resmod = Module("test", Seq(), Seq(impdef1, impdef2, impdef3, Goals(Seq(genTrueRule("test")), None)))
+
+    val res = ResolveImports(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(res.head == resmod)
+  }
+  
+  test("Correct Import simple dependencies - no module duplication, arbitrary import order") {
+    val impdef1 = genTestFunctionSig("f1")
+    val imp1 = Resolved(Module("ImportMe1", Seq(), Seq(impdef1)))
+
+    val impdef2 = genTestFunctionSig("f2")
+    val imp2 = Resolved(Module("ImportMe2", Seq(imp1), Seq(impdef2)))
+
+    val impdef3 = genTestFunctionSig("f3")
+    val imp3 = Resolved(Module("ImportMe3", Seq(imp2, imp1), Seq(impdef3)))
+
+    val mod = Module("test", Seq(imp3, imp1, imp2), Seq(Goals(Seq(genTrueRule("test")), None)))
+
+    val resmod = Module("test", Seq(), Seq(impdef1, impdef2, impdef3, Goals(Seq(genTrueRule("test")), None)))
+
+    val res = ResolveImports(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(res.head == resmod)
+  }
+  
+
+
 }
