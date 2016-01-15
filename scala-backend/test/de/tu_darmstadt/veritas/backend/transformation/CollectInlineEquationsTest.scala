82d8857d153b8b44c240bbad6a3b5ccdf2563530
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2016-01-15 14:04:29 +0100

    wrote more tests for collecting inline equations
    all work so far

--- scala-backend/test/de/tu_darmstadt/veritas/backend/transformation/CollectInlineEquationsTest.scala
+++ scala-backend/test/de/tu_darmstadt/veritas/backend/transformation/CollectInlineEquationsTest.scala
@@ -24,6 +24,78 @@ class CollectInlineEquationsTest extends FunSuite {
   def genAppApp1(fn: String, arg: FunctionExpMeta) =
     FunctionExpApp(fn, Seq(arg))
 
+  def genExists1(vars: List[String], arg: TypingRuleJudgment) =
+    ExistsJudgment(vars map { s => MetaVar(s) }, Seq(arg))
+
+  def genForall1(vars: List[String], arg: TypingRuleJudgment) =
+    ForallJudgment(vars map { s => MetaVar(s) }, Seq(arg))
+
+  test("Collect first instance only, if several given") {
+    val tr = TypingRule("simple",
+      Seq(genEq(genMeta("x"), genMeta("a")),
+        genEq(genMeta("x"), genMeta("b")),
+        genEq(genMeta("x"), genMeta("c"))),
+      Seq(FunctionExpJudgment(FunctionExpFalse)))
+    val mod = genSimpleModule("inlinesimple", tr)
+
+    val collection = CollectOnly(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(collection.head == mod)
+    assert(CollectOnly.chosenSubstitutions(MetaVar("x")) == genMeta("a"))
+    assert(CollectOnly.chosenSubstitutions.size == 1)
+  }
+  
+  test("Collect first instance only, if several given (in conclusion)") {
+    val tr = TypingRule("simple",
+      Seq(genEq(genMeta("x"), genMeta("a")),
+        genEq(genMeta("x"), genMeta("b"))),
+      Seq(genEq(genMeta("x"), genMeta("c"))))
+    val mod = genSimpleModule("inlinesimple", tr)
+
+    val collection = CollectOnly(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(collection.head == mod)
+    assert(CollectOnly.chosenSubstitutions(MetaVar("x")) == genMeta("a"))
+    assert(CollectOnly.chosenSubstitutions.size == 1)
+  }
+  
+  test("Collect several substitutions, both directions") {
+    val tr = TypingRule("simple",
+      Seq(genEq(genMeta("x"), genMeta("a")),
+        genEq(genApp1("g", "z"), genMeta("b")),
+        genEq(genMeta("z"), genMeta("c"))),
+      Seq(FunctionExpJudgment(FunctionExpFalse)))
+    val mod = genSimpleModule("inlinesimple", tr)
+
+    val collection = CollectOnly(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(collection.head == mod)
+    assert(CollectOnly.chosenSubstitutions(MetaVar("x")) == genMeta("a"))
+    assert(CollectOnly.chosenSubstitutions(MetaVar("b")) == genApp1("g", "z"))
+    assert(CollectOnly.chosenSubstitutions(MetaVar("z")) == genMeta("c"))
+    assert(CollectOnly.chosenSubstitutions.size == 3)
+  }
+  
+  test("Collect several substitutions, both directions, with conclusion") {
+    val tr = TypingRule("simple",
+      Seq(genEq(genMeta("x"), genMeta("a")),
+        genEq(genApp1("g", "z"), genMeta("b")),
+        genEq(genMeta("z"), genMeta("c"))),
+      Seq(genEq(genMeta("y"), genMeta("x")),
+          genEq(genMeta("c"), genMeta("b"))))
+    val mod = genSimpleModule("inlinesimple", tr)
+
+    val collection = CollectOnly(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(collection.head == mod)
+    assert(CollectOnly.chosenSubstitutions(MetaVar("x")) == genMeta("a"))
+    assert(CollectOnly.chosenSubstitutions(MetaVar("b")) == genApp1("g", "z"))
+    assert(CollectOnly.chosenSubstitutions(MetaVar("z")) == genMeta("c"))
+    assert(CollectOnly.chosenSubstitutions(MetaVar("y")) == genMeta("x"))
+    assert(CollectOnly.chosenSubstitutions(MetaVar("c")) == genMeta("b"))
+    assert(CollectOnly.chosenSubstitutions.size == 5)
+  }
+
   test("Collect only: Inlining within premises 'wrong' order") {
     val tr = TypingRule("simple",
       Seq(genEq(genMeta("z"), genApp1("f", "x")), genEq(genApp1("f", "y"), genMeta("x"))),
@@ -51,4 +123,74 @@ class CollectInlineEquationsTest extends FunSuite {
     assert(CollectOnly.chosenSubstitutions.size == 1)
   }
 
+  test("Collect in conclusion") {
+    val tr = TypingRule("simple",
+      Seq(),
+      Seq(genEq(genMeta("y"), genMeta("x")),
+        genEq(genApp1("g", "z"), genMeta("y"))))
+    val mod = genSimpleModule("inlinesimple", tr)
+
+    val collection = CollectOnly(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(collection.head == mod)
+    assert(CollectOnly.chosenSubstitutions(MetaVar("y")) == genMeta("x"))
+    assert(CollectOnly.chosenSubstitutions.size == 1)
+  }
+
+  test("Collect two things in conclusion") {
+    val tr = TypingRule("simple",
+      Seq(),
+      Seq(genEq(genMeta("y"), genMeta("x")),
+        genEq(genApp1("g", "z"), genMeta("a"))))
+    val mod = genSimpleModule("inlinesimple", tr)
+
+    val collection = CollectOnly(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(collection.head == mod)
+    assert(CollectOnly.chosenSubstitutions(MetaVar("y")) == genMeta("x"))
+    assert(CollectOnly.chosenSubstitutions(MetaVar("a")) == genApp1("g", "z"))
+    assert(CollectOnly.chosenSubstitutions.size == 2)
+  }
+
+  test("Correctly collect under presence of exists") {
+    val tr = TypingRule("simple",
+      Seq(genExists1(List("x", "z"), genEq(genMeta("x"), genMeta("z"))),
+        genEq(genMeta("x"), genMeta("y"))),
+      Seq(FunctionExpJudgment(FunctionExpFalse)))
+    val mod = genSimpleModule("inlinesimple", tr)
+
+    val collection = CollectOnly(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(collection.head == mod)
+    assert(CollectOnly.chosenSubstitutions(MetaVar("x")) == genMeta("y"))
+    assert(CollectOnly.chosenSubstitutions.size == 1)
+  }
+
+  test("Correctly collect under presence of forall") {
+    val tr = TypingRule("simple",
+      Seq(genForall1(List("x", "z"), genEq(genMeta("x"), genMeta("z"))),
+        genEq(genMeta("x"), genMeta("y"))),
+      Seq(FunctionExpJudgment(FunctionExpFalse)))
+    val mod = genSimpleModule("inlinesimple", tr)
+
+    val collection = CollectOnly(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(collection.head == mod)
+    assert(CollectOnly.chosenSubstitutions(MetaVar("x")) == genMeta("y"))
+    assert(CollectOnly.chosenSubstitutions.size == 1)
+  }
+  
+  test("Correctly collect under presence of not") {
+    val tr = TypingRule("simple",
+      Seq(NotJudgment(genEq(genMeta("x"), genMeta("z"))),
+        genEq(genMeta("x"), genMeta("y"))),
+      Seq(FunctionExpJudgment(FunctionExpFalse)))
+    val mod = genSimpleModule("inlinesimple", tr)
+
+    val collection = CollectOnly(Seq(mod))(Backend.onlyTFFTest)
+
+    assert(collection.head == mod)
+    assert(CollectOnly.chosenSubstitutions(MetaVar("x")) == genMeta("y"))
+    assert(CollectOnly.chosenSubstitutions.size == 1)
+  }
 }
