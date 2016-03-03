635bccd2afa975c850ad215ed7c1de0f51f6d6bb
Sebastian Erdweg <seba.github@gmail.com>
2016-03-03 10:48:34 +0100

    fixpoint trans

---
+++ scala-backend/src/de/tu_darmstadt/veritas/backend/transformation/Fixpoint.scala
@@ -0,0 +1,27 @@
+package de.tu_darmstadt.veritas.backend.transformation
+
+import de.tu_darmstadt.veritas.backend.Configuration
+import de.tu_darmstadt.veritas.backend.veritas.Module
+
+object Fixpoint {
+  val MAX = 1
+}
+
+case class Fixpoint(trans: ModuleTransformation) extends ModuleTransformation {
+  override def apply(m: Seq[Module])(implicit config: Configuration): Seq[Module] = {
+	  var it = 0
+	  def run(m: Seq[Module]): Seq[Module] = {
+		  if (it >= Fixpoint.MAX)
+			  m
+		  else {
+		    it += 1
+  		  val result = trans.apply(m)
+  		  if (result != m)
+  			  run(result)
+  		  else
+  			  result
+		  }
+	  }
+	  run(m)
+  }
+}
