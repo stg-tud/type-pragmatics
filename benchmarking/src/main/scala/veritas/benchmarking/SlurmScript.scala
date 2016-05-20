faf875064ce65b6f965c1fd96ee05677ed1edb32
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2016-05-20 15:11:05 +0200

    further improvements SlurmScripts
    distributed job arrays in batches of 1000

--- benchmarking/src/main/scala/veritas/benchmarking/SlurmScript.scala
+++ benchmarking/src/main/scala/veritas/benchmarking/SlurmScript.scala
@@ -10,12 +10,14 @@ import java.util.Date
   *
   * @param timeout       : in seconds
   * @param arraymaxindex : if script is a jobarray: maximum index of array (if not, pass 0)
-  * @param commands    : complete command for calling a prover
+  * @param commands      : complete command for calling a prover
   */
 case class SlurmScript(jobname: String, stdoutpath: String, stderrpath: String, timeout: Int, commands: String, arraymaxindex: Int = 0) {
 
   // fixed script values, adapt if necessary
   val scripttag = "#SBATCH"
+  val maxarraysize: Int = 1000
+  val filenumber = "filenumber"
 
   val projectname = "project00184"
   val mailuser = "grewe@st.informatik.tu-darmstadt.de"
@@ -35,54 +37,64 @@ case class SlurmScript(jobname: String, stdoutpath: String, stderrpath: String, 
     return formatter.format((new Date(timeout * 1000))) //Date takes timeout in milliseconds!
   }
 
-  def scriptString(): String = {
-    val scriptheader =
-      s"""#!/bin/bash
-         |$scripttag -J $jobname
-         |""".stripMargin
+  def scriptString(): Seq[String] = {
 
-    val arrayconf = if (arraymaxindex > 0)
-      s"$scripttag -a 1-$arraymaxindex"
-    else ""
+    //distribute array in batches with maximum size 1000
+    val minnumberOfScripts: Int = arraymaxindex / maxarraysize
+    val numberOfScripts: Int = if (minnumberOfScripts * 1000 <= arraymaxindex) minnumberOfScripts + 1 else minnumberOfScripts
 
-    val stdoutpathA = if (arraymaxindex > 0)
-      s"""${stdoutpath}%a"""
-    else
-      s"""$stdoutpath"""
+    for (i <- 0 until numberOfScripts) yield {
+      val scriptheader = s"""#!/bin/bash
+        |$scripttag -J $jobname
+        |""".stripMargin
 
-    val stderrpathA = if (arraymaxindex > 0)
-      s"""${stderrpath}%a"""
-    else
-      s"""$stderrpath"""
+      val localmaxindex = if ((i+1) < numberOfScripts) 1000 else arraymaxindex % maxarraysize
+      val arrayconf = if (arraymaxindex > 0)
+        s"""$scripttag -a 1-$localmaxindex
+          |""".stripMargin
+      else ""
 
-    val config =
-      s"""
-         |$scripttag -A $projectname
-         |$scripttag --mail-user=$mailuser
-         |$scripttag --mail-type=$mailtype
-         |$scripttag -o $stdoutpathA
-         |$scripttag -e $stderrpathA
-         |$scripttag -t ${timeoutToFormat()}
-         |$scripttag --mem-per-cpu=$mempercpu
-         |$scripttag -n $tasknumber
-         |$scripttag -c $corespertask
-         |$scripttag -C $features
-     """.stripMargin
+      val stdoutpathA = if (arraymaxindex > 0)
+          s"""${stdoutpath}_${i}_%a"""
+      else
+          s"""$stdoutpath"""
 
-    val benchmark = if (benchmark_conf != "") s"""\n$scripttag -p $benchmark_conf \n""" else ""
+      val stderrpathA = if (arraymaxindex > 0)
+        s"""${stderrpath}_${i}_%a"""
+      else
+        s"""$stderrpath"""
 
-    scriptheader + arrayconf + config + benchmark + commands + "\n"
+      val config =
+        s"""$scripttag -A $projectname
+           |$scripttag --mail-user=$mailuser
+           |$scripttag --mail-type=$mailtype
+           |$scripttag -o $stdoutpathA
+           |$scripttag -e $stderrpathA
+           |$scripttag -t ${timeoutToFormat()}
+           |$scripttag --mem-per-cpu=$mempercpu
+           |$scripttag -n $tasknumber
+           |$scripttag -c $corespertask
+           |$scripttag -C $features
+      """.stripMargin
+
+      val benchmark = if (benchmark_conf != "") s"""\n$scripttag -p $benchmark_conf \n""" else ""
+      val calcindex = if (arraymaxindex == 0) s"$filenumber=$$SLURM_ARRAY_TASK_ID\n" else s"$filenumber=$$(($i*1000+SLURM_ARRAY_TASK_ID))\n"
+      scriptheader + arrayconf + config + benchmark + calcindex + commands + "\n"
+    }
 
   }
 
   def writeScriptToFile(filepath: String) = {
-    val filehandler = new File(filepath)
-    if (!filehandler.getParentFile.exists())
-      filehandler.getParentFile.mkdirs()
-    filehandler.createNewFile()
-    new PrintWriter(filehandler) {
-      write(scriptString());
-      close
+    val scriptstrings = scriptString()
+    for (i <- 0 until scriptstrings.size) {
+      val filehandler = new File(filepath + "_" + i)
+      if (!filehandler.getParentFile.exists())
+        filehandler.getParentFile.mkdirs()
+      filehandler.createNewFile()
+      new PrintWriter(filehandler) {
+        write(scriptstrings(i));
+        close
+      }
     }
   }
 }
@@ -99,37 +111,36 @@ case class SlurmScriptMaker(proverconfigs: Seq[ProverConfig], provertimeout: Int
   val outputpathHHLR = "/work/scratch/groups/projects/proj_184/"
   val pathforHHLRJobscripts = "datasets/HHLRJobScripts/"
 
-  val arrayindexrefHHLR = "$SLURM_ARRAY_TASK_ID"
+  val arrayindexref = "filenumber"
 
   val timeoutbuffer = provertimeout * 2 //buffer time which is used when making scripts
 
   def writeFlattenedFileStructure() = {
-    for ((i, f) <- flatIndexFileMap)
-      {
-        val destfile = new File(s"$pathforHHLRInput$inputfilename$i")
-        if (!destfile.getParentFile.exists())
-          destfile.getParentFile.mkdirs()
-        new FileOutputStream(destfile) getChannel() transferFrom(new FileInputStream(f) getChannel, 0, Long.MaxValue)
-      }
+    for ((i, f) <- flatIndexFileMap) {
+      val destfile = new File(s"$pathforHHLRInput${inputfilename}_$i")
+      if (!destfile.getParentFile.exists())
+        destfile.getParentFile.mkdirs()
+      new FileOutputStream(destfile) getChannel() transferFrom(new FileInputStream(f) getChannel, 0, Long.MaxValue)
+    }
 
   }
 
   def writeJobScripts() = {
-    for (pc <- proverconfigs)
-      {
-        val jobsize = flatIndexFileMap.size
-        val arraymax = if (jobsize > 1) jobsize else 0
-        val jobname = pc.name
-        val stdoutpath = s"$outputpathHHLR$currentdate/${provertimeout}s/$jobname/${outputfilename}"
-        val stderrpath = stdoutpath
-        val inputpath = if (jobsize > 1) s"$inputpathHHLR$currentdate/${inputfilename}$arrayindexrefHHLR" else s"$inputpathHHLR$currentdate/${inputfilename}"
+    for (pc <- proverconfigs) {
+      val jobsize = flatIndexFileMap.size
+      val arraymax = if (jobsize > 1) jobsize else 0
+      val jobname = pc.name
+      val stdoutpath = s"$outputpathHHLR$currentdate/${provertimeout}s/$jobname/${outputfilename}"
+      val stderrpath = stdoutpath
+      val inputpath = if (jobsize > 1) s"$inputpathHHLR$currentdate/${inputfilename}_$$$arrayindexref" else s"$inputpathHHLR$currentdate/${inputfilename}"
 
-        val provercall = (pc.makeCall(new File(inputpath), provertimeout, false))
-        val provercallHHLR = proverpath + provercall(0).split("/").last + " " + provercall.drop(1).mkString(" ")
+      val provercall = (pc.makeCall(new File(inputpath), provertimeout, false))
+      val provercallHHLR = proverpath + provercall(0).split("/").last + " " + provercall.drop(1).mkString(" ")
 
-        val slurmjob = SlurmScript(jobname, stdoutpath, stderrpath, timeoutbuffer, provercallHHLR, arraymax)
-        slurmjob.writeScriptToFile(pathforHHLRJobscripts + jobname)
-      }
+
+      val slurmjob = SlurmScript(jobname, stdoutpath, stderrpath, timeoutbuffer, provercallHHLR, arraymax)
+      slurmjob.writeScriptToFile(pathforHHLRJobscripts + jobname)
+    }
   }
 
 }
