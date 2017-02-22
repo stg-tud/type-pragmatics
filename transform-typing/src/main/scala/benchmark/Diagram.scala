190d03e32971d366fd08a15d40410c8acc71ecc7
Sebastian Erdweg <seba.github@gmail.com>
2017-02-22 19:54:57 +0100

    prettier diagrams

--- transform-typing/src/main/scala/benchmark/Diagram.scala
+++ transform-typing/src/main/scala/benchmark/Diagram.scala
@@ -1,6 +1,6 @@
 package benchmark
 
-import java.io.{BufferedReader, File, FileReader}
+import java.io.{BufferedReader, File, FileReader, PrintWriter}
 
 import benchmark.Benchmark.OptimizationConfig
 import org.sameersingh.scalaplot.Style.{Color, PointType}
@@ -13,6 +13,21 @@ import scala.collection.mutable
 
 object Diagram extends App {
   case class Row(trans: String, config: OptimizationConfig.Value, obl: String, status: String, time: Double)
+
+  val realColors: Map[OptimizationConfig.Value, String] = Map(
+    OptimizationConfig.All -> "#fb8072",
+    OptimizationConfig.NoUnpack -> "#8dd3c7",
+    OptimizationConfig.NoNormalization -> "#dddd93",
+    OptimizationConfig.NoExistentialHints -> "#bebada",
+    OptimizationConfig.NoDropUnreachable -> "#80b1d3"
+  )
+  val encodedColors: Map[OptimizationConfig.Value, Color.Value] = Map(
+    OptimizationConfig.All -> Color.Black,
+    OptimizationConfig.NoUnpack -> Color.Grey,
+    OptimizationConfig.NoNormalization -> Color.Purple,
+    OptimizationConfig.NoExistentialHints -> Color.Magenta,
+    OptimizationConfig.NoDropUnreachable -> Color.Yellow
+  )
 
   def trimmed(s: String) = {
     val s2 = s.trim
@@ -65,12 +80,9 @@ object Diagram extends App {
 
       val series = new MemXYSeries(sorted ++ restime, percentiles ++ restprogres, config.toString)
       series.pointSize = Some(0.1)
-      series.lineWidth = Some(.3)
+      series.lineWidth = Some(1)
       series.pointType = Some(PointType.fullBox)
-      if (config == OptimizationConfig.All)
-        series.color = Some(Color.Magenta)
-      else if (config == OptimizationConfig.NoUnpack)
-        series.color = Some(Color.Grey)
+      series.color = Some(encodedColors(config))
       config -> series
     }.toSeq
     val sortedSeries = seriesPerConfig.sortBy(_._1).map(_._2)
@@ -89,10 +101,49 @@ object Diagram extends App {
     chart.showLegend = true
     chart.legendPosX = LegendPosX.Center
 
-    val plotter = new GnuplotPlotter(chart)
+    val plotter = new MyGnuplotPlotter(chart)
     plotter.pdf("benchmark/", s"$trans-chart")
   }
 
   for (file <- args)
     run(new File(file))
+}
+
+class MyGnuplotPlotter(chart: Chart) extends GnuplotPlotter(chart) {
+  override def writeScriptFile(directory: String, filenamePrefix: String, dummyTerminal: String,  filenameSuffix: String, stdout: Boolean = false, defaultTerminal: String = "dumb") {
+    val monochromeString = if (chart.monochrome) "monochrome" else ""
+    val sizeString = if (chart.size.isDefined) "size %f,%f" format(chart.size.get._1, chart.size.get._2) else ""
+    val terminal = "pdf enhanced linewidth 1.0 %s %s" format(monochromeString, sizeString)
+
+    // write the description
+    assert(new File(directory).isDirectory, directory + " should be a directory")
+    assert(directory.endsWith("/"), directory + " should end with a /")
+    reset
+    this.directory = directory
+    filename = filenamePrefix
+    plotChart(chart, terminal)
+    lines += "set terminal %s" format (terminal)
+    if (stdout) lines += "set output"
+    else lines += "set output \"%s\"" format (filename + "." + filenameSuffix)
+    chart match {
+      case xyc: XYChart => plotXYChart(xyc)
+      case bc: BarChart => plotBarChart(bc)
+    }
+    lines += "unset output"
+    lines += "# Wrapup"
+    lines += "set terminal %s" format (defaultTerminal)
+    lines += "refresh"
+
+    val scriptFile = directory + filenamePrefix + ".gpl"
+    val writer = new PrintWriter(scriptFile)
+    for (line <- lines) {
+      writer.println(line)
+    }
+    writer.close()
+  }
+
+  override protected def getColorname(color: Color.Type): String = {
+    import Diagram._
+    realColors(encodedColors.find(_._2 == color).get._1)
+  }
 }
