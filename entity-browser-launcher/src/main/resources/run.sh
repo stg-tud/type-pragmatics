c83c901ffeb9d781a6fddc878eeaaaef6b302265
Sebastian Erdweg <seba.github@gmail.com>
2017-03-09 15:35:07 +0100

    Squashed 'xodus-entity-browser/' content from commit 0119ed1
    git-subtree-dir: xodus-entity-browser
    git-subtree-split: 0119ed166f9af44ec4da7f052dbc21c4e356edd0

---
+++ entity-browser-launcher/src/main/resources/run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+SERVER_PORT=8080
+
+if [ -z "${JAVA_HOME}" ]; then
+  JAVA=java
+else
+  JAVA="${JAVA_HOME}/bin/java"
+  echo "info: Using java located in ${JAVA_HOME}."
+fi
+
+${JAVA} -Dlogback.configurationFile=logback.xml -Dserver.port=${SERVER_PORT} -Dentity.browser.config=./xodus-store.properties -Dexodus.entityStore.refactoring.skipAll=true -Dexodus.entityStore.cachingDisabled=true -jar jetty-runner.jar --port ${SERVER_PORT} ./resources/xodus-entity-browser.war
