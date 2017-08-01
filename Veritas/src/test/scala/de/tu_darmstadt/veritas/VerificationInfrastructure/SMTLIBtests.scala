0311b2cd447274bad8330c0b5314faa5eaac7067
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2017-08-01 16:21:01 +0200

    some more tests of SMTLIB translation

--- Veritas/src/test/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/SMTLIBtests.scala
+++ Veritas/src/test/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/SMTLIBtests.scala
@@ -1,6 +1,6 @@
 package de.tu_darmstadt.veritas.VerificationInfrastructure
 
-import de.tu_darmstadt.veritas.VerificationInfrastructure.verifier.{ADTVampireVerifier, TPTPVampireVerifier, Verifier}
+import de.tu_darmstadt.veritas.VerificationInfrastructure.verifier.{ADTVampireVerifier, TPTPVampireVerifier, Verifier, Z3VampireVerifier}
 import de.tu_darmstadt.veritas.backend.ast.VeritasConstruct
 import org.scalatest.FunSuite
 
@@ -16,10 +16,11 @@ class SMTLIBtests extends FunSuite {
   val vampire_tar = new ADTVampireVerifier()
 
   val defaultshort_timeout = 5
-  val defaultlong_timeout = 10
+  val defaultlong_timeout = 120
   val unsuccessful_timeout = 1
 
   def makeCustomVampireTar(timeout: Int) = new ADTVampireVerifier(timeout)
+  def makeCustomVampireZ3(timeout: Int) = new Z3VampireVerifier(timeout)
   def makeCustomVampire(timeout: Int) = new TPTPVampireVerifier(timeout)
 
   def successfulVerification(ps: loaded_g.ProofStep,
@@ -32,6 +33,7 @@ class SMTLIBtests extends FunSuite {
   def inconclusiveVerification(ps: loaded_g.ProofStep,
                                v: Verifier[VeritasConstruct, VeritasConstruct]): Unit = {
     val res = loaded_g.verifyProofStep(ps, v)
+    println(res.status) //for debugging
     assert(!res.status.isVerified)
     assert(res.errorMsg.nonEmpty)
     assert(res.errorMsg.get == "Time limit")
@@ -40,6 +42,7 @@ class SMTLIBtests extends FunSuite {
   test("Successful verification of Tvalue base case") {
     successfulVerification(tvaluecasePS, makeCustomVampire(defaultshort_timeout))
     successfulVerification(tvaluecasePS, makeCustomVampireTar(defaultshort_timeout))
+    successfulVerification(tvaluecasePS, makeCustomVampireZ3(defaultshort_timeout))
   }
 
   test("Try verifying selectFromWhere case top-level (inconclusive)") {
@@ -65,24 +68,34 @@ class SMTLIBtests extends FunSuite {
   //difference to normal Vampire 4.1 using untyped logic....! (with typed logic, it also does not work)
   test("Unsuccessful verification of union tvalue-tvalue") {
     val ps = loaded_g.appliedStep(loaded_g.requiredObls(unioncasePS).head._1).get
-    inconclusiveVerification(ps, makeCustomVampire(unsuccessful_timeout))
-    inconclusiveVerification(ps, makeCustomVampireTar(unsuccessful_timeout))
+    inconclusiveVerification(ps, makeCustomVampire(defaultlong_timeout))
+    inconclusiveVerification(ps, makeCustomVampireTar(defaultlong_timeout))
   }
 
   test("Unsuccessful verification of intersection tvalue-tvalue") {
     val ps = loaded_g.appliedStep(loaded_g.requiredObls(intersectioncasePS).head._1).get
-    inconclusiveVerification(ps, makeCustomVampire(unsuccessful_timeout))
-    inconclusiveVerification(ps, makeCustomVampireTar(unsuccessful_timeout))
+    inconclusiveVerification(ps, makeCustomVampire(defaultlong_timeout))
+    inconclusiveVerification(ps, makeCustomVampireTar(defaultlong_timeout))
   }
 
   test("Unsuccessful verification of difference tvalue-tvalue") {
     val ps = loaded_g.appliedStep(loaded_g.requiredObls(differencecasePS).head._1).get
-    inconclusiveVerification(ps, makeCustomVampire(unsuccessful_timeout))
-    inconclusiveVerification(ps, makeCustomVampireTar(unsuccessful_timeout))
+    inconclusiveVerification(ps, makeCustomVampire(defaultlong_timeout))
+    inconclusiveVerification(ps, makeCustomVampireTar(defaultlong_timeout))
   }
 
   test("verification of more difficult set sub-cases") {
+    val unsuccessfulObls = loaded_g.requiredObls(unioncasePS).tail ++
+      loaded_g.requiredObls(intersectioncasePS).tail ++ loaded_g.requiredObls(differencecasePS).tail
 
+    for ((o, e) <- unsuccessfulObls) {
+      val stepO = loaded_g.appliedStep(o)
+      assert(stepO.nonEmpty)
+      val step = stepO.get
+      inconclusiveVerification(step, makeCustomVampire(defaultlong_timeout))
+      inconclusiveVerification(step, makeCustomVampireTar(defaultlong_timeout))
+
+    }
   }
 
 
