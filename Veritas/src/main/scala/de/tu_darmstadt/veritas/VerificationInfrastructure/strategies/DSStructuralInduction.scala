a44f5f663ede9d779a934bd48f7735f32b1a55bb
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2017-08-28 18:44:11 +0200

    refine structural induction tactic and DS-Strategy

--- Veritas/src/main/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/strategies/DSStructuralInduction.scala
+++ Veritas/src/main/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/strategies/DSStructuralInduction.scala
@@ -2,8 +2,13 @@ package de.tu_darmstadt.veritas.VerificationInfrastructure.strategies
 
 import de.tu_darmstadt.veritas.VerificationInfrastructure.{ProofGraph, ProofGraphTraversals}
 import de.tu_darmstadt.veritas.VerificationInfrastructure.domaindescription.DomainDescription
+import de.tu_darmstadt.veritas.VerificationInfrastructure.tactic.StructuralInduction
 
-case class DSStructuralInduction[Spec, Goal, I, Repr](inductionvar : I, domaindescription: DomainDescription[Repr]) extends DSStrategy[Spec, Goal] {
+case class DSStructuralInduction[Spec <: Ordered[Spec], Goal <: Ordered[Goal], Repr](spec: Spec, inductionvar : Spec, domaindescription: DomainDescription[Repr]) extends DSStrategy[Spec, Goal] {
 
-  override def applyToPG(pg: ProofGraph[Spec, Goal] with ProofGraphTraversals[Spec, Goal])(obl: pg.Obligation): ProofGraph[Spec, Goal] = ???
+  override def applyToPG(pg: ProofGraph[Spec, Goal] with ProofGraphTraversals[Spec, Goal])(obl: pg.Obligation): ProofGraph[Spec, Goal] = {
+    //TODO: create the induction hypotheses, and the induction cases
+    pg.applyTactic(obl, StructuralInduction[Spec, Goal](spec, ??? /*all the induction cases*/))
+    pg
+  }
 }
--- Veritas/src/main/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/tactic/StructuralInduction.scala
+++ Veritas/src/main/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/tactic/StructuralInduction.scala
@@ -3,7 +3,7 @@ package de.tu_darmstadt.veritas.VerificationInfrastructure.tactic
 import de.tu_darmstadt.veritas.VerificationInfrastructure._
 import de.tu_darmstadt.veritas.VerificationInfrastructure.verifier.Verifier
 
-case class StructuralInduction[Spec <: Ordered[Spec], Goal <: Ordered[Goal]](inductionvar: Spec) extends Tactic[Spec, Goal] {
+case class StructuralInduction[Spec <: Ordered[Spec], Goal <: Ordered[Goal]](spec: Spec, cases: Seq[(Goal, StructInductCase[Spec])]) extends Tactic[Spec, Goal] {
   //TODO we might have to refine the verifier call for induction once we really support this via a prover
   override def verifyStep[Result <: GenStepResult[Spec, Goal]](obl: GenObligation[Spec, Goal],
                                                                parentedges: Iterable[EdgeLabel],
@@ -15,14 +15,28 @@ case class StructuralInduction[Spec <: Ordered[Spec], Goal <: Ordered[Goal]](ind
 
 
   override def compare(that: Tactic[Spec, Goal]): Int = that match {
-    case that: StructuralInduction[Spec, Goal] => this.inductionvar compare that.inductionvar
+    case that: StructuralInduction[Spec, Goal] => {
+      val caseslengthcomp = this.cases.length compare that.cases.length
+      if (caseslengthcomp == 0) {
+        //compare individual cases against each other until first difference found
+        lazy val individualcomp = for (((s1, e1), (s2, e2)) <- this.cases zip that.cases) yield (s1 compare s2, e1 compare e2)
+        val comppair = individualcomp.find(_ != (0, 0)).getOrElse((0, 0))
+        if (comppair._1 == 0) comppair._2 else comppair._1
+      }
+      else
+        caseslengthcomp
+
+    }
     case _ => this.getClass.getCanonicalName.compare(that.getClass.getCanonicalName)
   }
 
   override def apply[Obligation](obl: GenObligation[Spec, Goal],
                                  obllabels: Iterable[EdgeLabel],
                                  produce: ObligationProducer[Spec, Goal, Obligation]): Iterable[(Obligation, EdgeLabel)] =
-    ???
+    for ((sobl, edge) <- cases) yield {
+      val newobl = produce.newObligation(spec, sobl)
+      (newobl, edge)
+    }
 }
 
 // D: format for block of variable/constant declarations
@@ -45,9 +59,8 @@ case class InductionHypotheses[A <: Ordered[A]](ihs: A) extends PropagatableInfo
   * @param fixedvars variables that need to fixed so that they explicitly refer to the same variables in the ihs and in the goal
   * @param ihs       induction hypotheses
   */
-case class StructInductCase[Spec <: Ordered[Spec]](casename: String,
-                                                                          fixedvars: Option[FixedVars[Spec]],
-                                                                          ihs: InductionHypotheses[Spec]) extends EdgeLabel {
+case class StructInductCase[Spec <: Ordered[Spec]](casename: String, fixedvars: Option[FixedVars[Spec]],
+                                                   ihs: InductionHypotheses[Spec]) extends EdgeLabel {
 
   override def desc: String = casename
 
