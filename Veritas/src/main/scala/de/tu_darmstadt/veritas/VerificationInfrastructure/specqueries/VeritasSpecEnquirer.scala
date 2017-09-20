d05cd3e5cf4153d1c32d4e127776a7c85724ac13
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2017-09-20 19:52:50 +0200

    started implementing spec query interface
    PROBLEM: second type parameter should be refined to VeritasFormula (or a construct grouping goals/lemmas etc.) but currently, it’s not easy to introduce such a trait that satisfies the required parameter bounds

---
+++ Veritas/src/main/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/specqueries/VeritasSpecEnquirer.scala
@@ -0,0 +1,60 @@
+package de.tu_darmstadt.veritas.VerificationInfrastructure.specqueries
+
+import de.tu_darmstadt.veritas.VerificationInfrastructure.tactics.FixedVar
+import de.tu_darmstadt.veritas.backend.ast.VeritasConstruct
+
+class VeritasSpecEnquirer extends SpecEnquirer[VeritasConstruct, VeritasConstruct] {
+
+  override def isRecursiveFunction(functioncall: VeritasConstruct): Boolean = ???
+
+  override def isClosedADT(v: VeritasConstruct) = ???
+
+  override def isForall(g: VeritasConstruct) = ???
+
+  override def isExists(g: VeritasConstruct) = ???
+
+  override def isImplication(g: VeritasConstruct) = ???
+
+  /**
+    * receive a formula that is universally or existentially quantified, return the body of the formula
+    *
+    * @param quantifiedBody
+    * @return
+    */
+  override def getQuantifiedBody(quantifiedBody: VeritasConstruct) = ???
+
+  override def getArguments(functioncall: VeritasConstruct) = ???
+
+  override def getCases(v: VeritasConstruct) = ???
+
+  override def getRecArgsADT(c: VeritasConstruct) = ???
+
+  override def getUniversallyQuantifiedVars(g: VeritasConstruct) = ???
+
+  override def getPremises(g: VeritasConstruct) = ???
+
+  override def getConclusions(g: VeritasConstruct) = ???
+
+  override def getVars(varblock: VeritasConstruct) = ???
+
+  override def getFormulae(formblock: VeritasConstruct) = ???
+
+  override def getFormulaName(f: VeritasConstruct) = ???
+
+  override def extractFunctionCalls(s: VeritasConstruct) = ???
+
+  override def extractFreeVariables(d: VeritasConstruct) = ???
+
+  override def consolidateFreeVariableNames[D <: VeritasConstruct](nd: D, refd: D) = ???
+
+  override def makeForall(vars: Seq[VeritasConstruct], body: VeritasConstruct) = ???
+
+  override def makeForallQuantifyFreeVariables(body: VeritasConstruct, fixed: Seq[FixedVar[VeritasConstruct]]) = ???
+
+  override def makeImplication(prems: Seq[VeritasConstruct], concs: Seq[VeritasConstruct]) = ???
+
+  override def makeEquation(left: VeritasConstruct, right: VeritasConstruct) = ???
+
+  override def makeNamedFormula(f: VeritasConstruct, name: String) = ???
+
+}
