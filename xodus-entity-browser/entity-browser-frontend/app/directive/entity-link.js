32ce963f2c60472765ce5fc366587ca9c2338656
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2018-03-21 12:04:03 +0100

    erased xodus entity browser (due to security problems)

--- xodus-entity-browser/entity-browser-frontend/app/directive/entity-link.js
+++
@@ -1,8 +0,0 @@
-angular.module('xodus').directive('entityLink', function () {
-    return {
-        restrict: 'E',
-        template: function (element, attrs) {
-            return '<a href="#/type/' + attrs.typeid + '/entity/' + attrs.entityid + '">' + attrs.title + '</a>';
-        }
-    };
-});
