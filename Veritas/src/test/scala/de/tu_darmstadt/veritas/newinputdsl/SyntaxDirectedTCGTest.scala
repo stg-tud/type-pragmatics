f7650e8d5f6971b79dccb691de72f5ea7d58c9b7
Andre Pacak <andre.pacak@googlemail.com>
2018-03-26 08:28:10 +0200

    simple failed tests

--- Veritas/src/test/scala/de/tu_darmstadt/veritas/newinputdsl/SyntaxDirectedTCGTest.scala
+++ Veritas/src/test/scala/de/tu_darmstadt/veritas/newinputdsl/SyntaxDirectedTCGTest.scala
@@ -7,11 +7,11 @@ import de.tu_darmstadt.veritas.newinputdsl.QLSpec._
 import org.scalatest.FunSuite
 
 class SyntaxDirectedTCGTest extends FunSuite {
-  val qlSpecFile =
+  private val qlSpecFile =
     new File("src/test/scala/de/tu_darmstadt/veritas/newinputdsl/QLSpec.scala")
-  val qlSpecString = scala.io.Source.fromFile(qlSpecFile).mkString("")
-  val generator = new SyntaxDirectedTypeCheckerGenerator[QLSpec.type, QLSpec.Context, QLSpec.Expression, QLSpec.Type] {}
-  val typechecker = generator.generate(qlSpecString)
+  private val qlSpecString = scala.io.Source.fromFile(qlSpecFile).mkString("")
+  private val generator = new SyntaxDirectedTypeCheckerGenerator[QLSpec.type, QLSpec.Context, QLSpec.Expression, QLSpec.Type] {}
+  private val typechecker = generator.generate(qlSpecString)
 
   test ("simple qseq") {
     assert(typechecker.typable(
@@ -27,6 +27,13 @@ class SyntaxDirectedTCGTest extends FunSuite {
       MC(atmbind(someID(zero()), YesNo(), atmempty()), atmempty())))
   }
 
+  test ("failed simple ask") {
+    assert(!typechecker.typable(
+      MC(atmempty(), atmempty()),
+      qsingle(ask(someID(zero()))),
+      MC(atmbind(someID(zero()), YesNo(), atmempty()), atmempty())))
+  }
+
   test ("simple question") {
     assert(typechecker.typable(
       MC(atmempty(), atmempty()),
@@ -34,11 +41,25 @@ class SyntaxDirectedTCGTest extends FunSuite {
       MC(atmbind(someID(zero()), YesNo(), atmempty()), atmempty())))
   }
 
+  test ("failed simple question") {
+    assert(!typechecker.typable(
+      MC(atmempty(), atmempty()),
+      qsingle(question(someID(zero()), someLabel(zero()), YesNo())),
+      MC(atmbind(someID(zero()), Number(), atmempty()), atmempty())))
+  }
+
   test ("simple value") {
     assert(typechecker.typable(
       MC(atmempty(), atmempty()),
       qsingle(value(someID(zero()), YesNo(), binop(constant(B(no())), eqop(), constant(B(yes()))))),
       MC(atmbind(someID(zero()), YesNo(), atmempty()), atmempty())))
+  }
+
+  test ("failed simple value") {
+    assert(!typechecker.typable(
+      MC(atmempty(), atmempty()),
+      qsingle(value(someID(zero()), YesNo(), binop(constant(B(no())), eqop(), constant(B(yes()))))),
+      MC(atmbind(someID(zero()), YesNo(), atmempty()), atmbind(someID(succ(zero())), Text(), atmempty()))))
   }
 
   test ("simple defquestion") {
