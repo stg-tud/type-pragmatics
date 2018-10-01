8f67fdc5f6bb9316a71583c4da2c0aa571494b22
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2018-10-01 23:26:30 +0200

    developed progress graph for AE further in code

--- Veritas/src/test/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/AESoundnessProofGraph.scala
+++ Veritas/src/test/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/AESoundnessProofGraph.scala
@@ -4,6 +4,7 @@ import java.io.{File, PrintWriter}
 
 import de.tu_darmstadt.veritas.VerificationInfrastructure.specqueries.VeritasSpecEnquirer
 import de.tu_darmstadt.veritas.VerificationInfrastructure.tactics.{Solve, StructuralInduction}
+import de.tu_darmstadt.veritas.VerificationInfrastructure.verifier._
 import de.tu_darmstadt.veritas.VerificationInfrastructure.visualizer.Dot
 import de.tu_darmstadt.veritas.backend.ast._
 import de.tu_darmstadt.veritas.backend.util.prettyprint.PrettyPrintWriter
@@ -44,10 +45,55 @@ class AESoundnessProofGraph(storefile: File) {
   val progressObligation: g.Obligation = g.newObligation(fullAESpec, Goals(Seq(progress_tr), None))
   g.storeObligation("Progress", progressObligation)
 
+  //apply structural induction on a given induction var to a given obligation and retrieve all resulting obligations
+  def applyInductionGetCases(obl: g.Obligation, indvar: MetaVar): Map[String, (g.Obligation, EdgeLabel)] = {
+    val indtac = StructuralInduction(indvar, fullAESpec, specenq)
+    val ps = g.applyTactic(obl, indtac)
+    val subobls = g.requiredObls(ps)
+    indtac.enumerateCases(subobls)
+  }
+
+  // mutable function: apply Solve tactic to all sub-obligations of a given obligation and return the resulting proof steps
+  def applySolveToAllSub(obl: g.Obligation): Seq[g.ProofStep] = {
+    g.appliedStep(obl) match {
+      case Some(ps) => {
+        val subobls = g.requiredObls(ps).map(_._1)
+        (for (o <- subobls) yield g.applyTactic(o, Solve[VeritasConstruct, VeritasFormula])).toSeq
+      }
+      case None => Seq()
+    }
+
+  }
+
   //apply top-level induction
-  private val rootInduction = StructuralInduction(MetaVar("t1"), fullAESpec, specenq)
-  val rootinductionPS: g.ProofStep = g.applyTactic(progressObligation, rootInduction)
-  addSolveTacticToAllLeaves()
+  val rootobl = g.findObligation("Progress").get
+  val rootobl_edge_map = applyInductionGetCases(rootobl, MetaVar("t1"))
+
+  val steps = applySolveToAllSub(rootobl)
+
+  val simpleVampire4_1 = new TPTPVampireVerifier(5)
+  val simpleVampire4_1_20 = new TPTPVampireVerifier(20)
+
+  val stepresults = for (ps <- steps) yield g.verifyProofStep(ps, simpleVampire4_1)
+
+  // print some results for debugging
+//  for (sr <- stepresults) sr.status match {
+//    case Finished(Proved(ATPResultDetails(_,_, Some(proof), Some(lemmas),_)), _) =>
+//      println("Proof: " +  proof)
+//      println("Used lemmas: " + lemmas)
+//    case s => println(s)
+//  }
+
+
+
+  //retrieve Ifelse-case
+  val ifcase_obl = rootobl_edge_map("ProgressIfelse")._1
+
+  def checkConsistency(): Unit = {
+    val vampireProver = VampireTPTP("4.1", 120)
+    val checker = ConsistencyChecker(VeritasTransformerBestStrat.config, vampireProver)
+    println(checker.consistent(fullAESpec))
+  }
 
 }
 
@@ -67,11 +113,25 @@ object ConstructAESoundnessGraph extends App {
   val pg = new AESoundnessProofGraph(file)
   val PG_UI = new ProofGraphUI[VeritasConstruct, VeritasFormula](pg.g, ProofGraphUI.extractGoalOrLemmaName)
 
+
   //print progress obligation in ScalaSPL (using ScalaSPL pretty printer)
   val prettyPrinter = new SimpleToScalaSPLSpecificationPrinter {
     override val printer: PrettyPrintWriter = new PrettyPrintWriter(new PrintWriter(System.out))
   }
+
+  //pg.checkConsistency() //nothing found with timeout 120 sec
+
   prettyPrinter.printTypingRule(pg.progress_tr)
+
+  val ifcase_tr: TypingRule = pg.ifcase_obl.goal match {
+    case Goals(Seq(tr),_) => tr
+    case t@TypingRule(_, _, _ ) => t
+  }
+
+
+  prettyPrinter.printTypingRule(ifcase_tr)
+  //val ifelseobl_ui = PG_UI.getObligation("ProgressIfelse")
+  //println(PG_UI.getAssembledProblem(ifelseobl_ui, VeritasTransformerBestStrat))
   //since we call pretty printing only for a typing rule and not for an entire model, we need to close the writer
   // manually in order to see the remaining parts of the typing rule printed
   prettyPrinter.printer.close()
@@ -84,5 +144,7 @@ object ConstructAESoundnessGraph extends App {
   }
 
   visualizeGraph("ae_soundness.png")
+
+
 
 }
