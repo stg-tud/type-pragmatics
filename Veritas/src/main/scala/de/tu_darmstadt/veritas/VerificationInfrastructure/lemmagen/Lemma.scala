37969a52950875da908ddefd2cff952673ff656d
Friedrich Weber <fweber@posteo.de>
2018-10-19 21:52:09 +0200

    Generate predicates as premises

--- Veritas/src/main/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/lemmagen/Lemma.scala
+++ Veritas/src/main/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/lemmagen/Lemma.scala
@@ -1,10 +1,15 @@
 package de.tu_darmstadt.veritas.VerificationInfrastructure.lemmagen
 
-import de.tu_darmstadt.veritas.backend.ast.{MetaVar, SortRef, TypingRule}
+import de.tu_darmstadt.veritas.backend.ast.{MetaVar, SortRef, TypingRule, TypingRuleJudgment}
 
 class Lemma(val bindings: Map[MetaVar, SortRef], val rule: TypingRule) {
   def boundTypes: Set[SortRef] = bindings.values.toSet
-  def bind(variable: MetaVar, typ: SortRef) = new Lemma(bindings + (variable -> typ), rule)
+  def bind(variables: MetaVar*) = new Lemma(bindings ++ variables.map(v => (v, v.sortType)), rule)
+  def withPremise(premise: TypingRuleJudgment) =
+    new Lemma(
+      bindings,
+      TypingRule(rule.name, premise +: rule.premises, rule.consequences)
+    )
 }
 
 object Lemma {
--- Veritas/src/main/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/lemmagen/LemmaGenerator.scala
+++ Veritas/src/main/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/lemmagen/LemmaGenerator.scala
@@ -4,6 +4,7 @@ import java.io.File
 
 import de.tu_darmstadt.veritas.backend.ast.function.{FunctionDef, FunctionExpApp, FunctionExpMeta, FunctionMeta}
 import de.tu_darmstadt.veritas.backend.ast._
+import de.tu_darmstadt.veritas.backend.transformation.collect.TypeInference.Sort
 import de.tu_darmstadt.veritas.scalaspl.dsk.DomainSpecificKnowledgeBuilder
 import de.tu_darmstadt.veritas.scalaspl.translator.ScalaSPLTranslator
 
@@ -16,9 +17,11 @@ class LemmaGenerator(specFile: File) {
 
   private var ctr = 0
 
-  def generateMetaVar(): MetaVar = {
+  def generateMetaVar(typ: SortRef): MetaVar = {
+    val meta = MetaVar(s"x$ctr")
     ctr += 1
-    MetaVar(s"x$ctr")
+    meta.typ = Some(Sort(typ.name))
+    meta
   }
 
   def evolveProgressLemma(lemma: Lemma): Unit = {
@@ -26,9 +29,9 @@ class LemmaGenerator(specFile: File) {
   }
 
   def buildSuccessLemma(function: FunctionDef): Lemma = {
-    val arguments = function.signature.in.map(ref => (generateMetaVar(), ref))
+    val arguments = function.signature.in.map(ref => (generateMetaVar(ref), ref))
     val (_, successConstructor) = enquirer.retrieveFailableConstructors(function.signature.out)
-    val successVar = generateMetaVar()
+    val successVar = generateMetaVar(function.signature.out)
     val invocationExp = FunctionExpApp(function.signature.name, arguments.map {
       case (name, typ) => FunctionMeta(name)
     })
@@ -38,26 +41,33 @@ class LemmaGenerator(specFile: File) {
     new Lemma(arguments.toMap, TypingRule(s"${function.signature.name}Progress", Seq(), Seq(exists)))
   }
 
+  def selectPredicate(baseLemma: Lemma, predicate: FunctionDef): Lemma = {
+    val (boundTypes, unboundTypes) = predicate.signature.in.partition(baseLemma.boundTypes.contains(_))
+    val newMetaVars = unboundTypes.map(generateMetaVar)
+    var lemma = baseLemma.bind(newMetaVars:_*)
+    // collect vars of suitable type for invocation TODO: there might be choices here
+    val arguments = predicate.signature.in.map(typ => lemma.bindings.find(_._2 == typ).get._1)
+    val invocationExp = FunctionExpJudgment(
+      FunctionExpApp(
+        predicate.signature.name,
+        arguments.map(v => FunctionMeta(v))
+      )
+    )
+    lemma.withPremise(invocationExp)
+  }
+
   def generateProgressLemma(dynamicFunctionName: String): Lemma = {
     val dynamicFunction = dsk.dynamicFunctions.find(_.signature.name == dynamicFunctionName).get
     // the function's return type must be failable
     var lemma = buildSuccessLemma(dynamicFunction)
-lemma
     // build a map of predicates and producers of "in types"
-    /*val predicates = Map(inTypes.map(ref => (ref, enquirer.retrievePredicates(ref))):_*)
-    val producers = Map(inTypes.map(ref => (ref, enquirer.retrieveProducers(ref))):_*)
-    val failableProducers = producers.map {
-      case (ref, defs) => (ref, defs.filter(defn => enquirer.isFailableType(defn.signature.out)))
-    }.toMap
-
-
-
-
+    val predicates = lemma.boundTypes.flatMap(enquirer.retrievePredicates)
+    val producers = lemma.boundTypes.flatMap(enquirer.retrieveProducers)
+    val failableProducers = producers.filter(defn => enquirer.isFailableType(defn.signature.out))
     // we just have to find matching premises
     // find predicates that involve any of the given types
-    val matchingPredicates = getBoundTypes().flatMap(predicates(_))
-    val matchingFailableProducers = getBoundTypes().flatMap(failableProducers(_))
-    println(matchingPredicates)
-    println(matchingFailableProducers)*/
+    println(predicates.map(_.signature.name))
+    println(failableProducers.map(_.signature.name))
+    selectPredicate(lemma, predicates.head)
   }
 }
