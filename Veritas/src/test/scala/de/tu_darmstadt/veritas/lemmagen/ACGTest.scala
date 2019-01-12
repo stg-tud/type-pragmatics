0c40d1215a837201df9698d9cffda412b8b40ec8
Friedrich Weber <fweber@posteo.de>
2019-01-12 09:33:27 +0100

    Add function call graph generator

--- Veritas/src/test/scala/de/tu_darmstadt/veritas/lemmagen/ACGTest.scala
+++ Veritas/src/test/scala/de/tu_darmstadt/veritas/lemmagen/ACGTest.scala
@@ -1,13 +1,15 @@
 package de.tu_darmstadt.veritas.lemmagen
 
-import java.io.{File, PrintWriter}
+import java.io.{BufferedWriter, File, FileWriter, PrintWriter}
 
-import de.tu_darmstadt.veritas.VerificationInfrastructure.lemmagen.Problem
+import de.tu_darmstadt.veritas.VerificationInfrastructure.lemmagen.{FunctionCallGraphGenerator, Problem}
 import de.tu_darmstadt.veritas.backend.ast.function.{FunctionDef, FunctionEq, FunctionExp, FunctionExpMeta}
 import de.tu_darmstadt.veritas.backend.util.prettyprint.PrettyPrintWriter
 import de.tu_darmstadt.veritas.scalaspl.prettyprint.SimpleToScalaSPLSpecificationPrinter
 import de.tu_darmstadt.veritas.scalaspl.util.{AugmentedCallGraph, VeritasAugmentedCallGraph, VeritasAugmentedCallGraphBuilder}
 import org.scalatest.FunSuite
+
+import scala.sys.process.stringToProcess
 
 class ACGTest extends FunSuite {
   val Directory = new File("augmented-call-graphs")
@@ -21,10 +23,10 @@ class ACGTest extends FunSuite {
     file.delete
   }
 
-  if(Directory.exists()) recursivedelete(Directory)
+  if (Directory.exists()) recursivedelete(Directory)
   Directory.mkdirs()
 
-  def generateACG(function: FunctionDef): AugmentedCallGraph[FunctionEq, FunctionExp, FunctionExpMeta]  = {
+  def generateACG(function: FunctionDef): AugmentedCallGraph[FunctionEq, FunctionExp, FunctionExpMeta] = {
     new VeritasAugmentedCallGraphBuilder(problem.spec).translate(function)(
       VeritasAugmentedCallGraph(function.signature.name)
     )
@@ -35,6 +37,19 @@ class ACGTest extends FunSuite {
       val acg = generateACG(fn)
       acg.visualizeACG(new File(Directory, s"acg-${fn.signature.name}.png"))
     }
+  }
+
+  test("make inter-function call graph") {
+    val reduce = dsk.lookupByFunName(dsk.dynamicFunctions, "reduce").get
+    val fct = new FunctionCallGraphGenerator(problem).generate(reduce)
+    val dotFile = new File(Directory, "function-call-tree.dot")
+    val writer = new BufferedWriter(new FileWriter(dotFile))
+    writer.write(FunctionCallGraphGenerator.makeDotString(fct))
+    writer.close()
+    val outFile = new File(Directory, "function-call-tree.png")
+    val exitCode = s"dot -Tpng ${dotFile.getAbsolutePath} -o${outFile.getAbsolutePath}".!
+    if (exitCode != 0)
+      throw new RuntimeException("Function Call Graph could not be visualized. This could be caused by the non-existence of the dot command.")
   }
 }
 
