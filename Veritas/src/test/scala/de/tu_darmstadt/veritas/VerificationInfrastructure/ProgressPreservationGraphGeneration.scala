262c9beaf123a66fa1214451ee1fe7895920161c
Sylvia Grewe <grewe@st.informatik.tu-darmstadt.de>
2019-02-26 13:50:21 +0100

    test code for case studies

--- Veritas/src/test/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/ProgressPreservationGraphGeneration.scala
+++ Veritas/src/test/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/ProgressPreservationGraphGeneration.scala
@@ -10,6 +10,24 @@ object ProgressPreservationGraphGeneration extends App {
   val AEProof = ProgressPreservationProofGraphGeneration(aesource, aestore)
 
   AEProof.visualizeProofGraph("GeneratedAESoundness_Unverified.png")
-  AEProof.verifyAll()
-  AEProof.visualizeProofGraph("GeneratedAESoundness_PartialVerification.png")
+  //AEProof.verifyAll()
+  //AEProof.visualizeProofGraph("GeneratedAESoundness_PartialVerification.png")
+
+  //val sqlsource = "src/test/scala/de/tu_darmstadt/veritas/scalaspl/SQLSpec.scala"
+  //val sqlstore = "SQLSoundnessGeneratedProofGraph-store"
+
+  //val SQLProof = ProgressPreservationProofGraphGeneration(sqlsource, sqlstore)
+
+  //SQLProof.visualizeProofGraph("GeneratedSQLSoundness_Unverified.png")
+  //SQLProof.verifyAll()
+  //SQLProof.visualizeProofGraph("GeneratedSQLSoundness_PartialVerification.png")
+
+  //val qlsource = "src/test/scala/de/tu_darmstadt/veritas/scalaspl/QLSpec.scala"
+  //val qlstore = "QLSoundnessGeneratedProofGraph-store"
+
+  //val QLProof = ProgressPreservationProofGraphGeneration(qlsource, qlstore)
+
+  //QLProof.visualizeProofGraph("GeneratedQLSoundness_Unverified.png")
+  //QLProof.verifyAll()
+  //QLProof.visualizeProofGraph("GeneratedQLSoundness_PartialVerification.png")
 }
