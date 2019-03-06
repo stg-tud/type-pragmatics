d1d8d9b17a42a904b8b14a450de769f22189dcdc
Friedrich Weber <fweber@posteo.de>
2019-03-06 23:17:30 +0100

    Reformulate code to be closer to thesis

--- Veritas/src/main/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/lemmagen/clever/construction/LemmaGraphConstructor.scala
+++ Veritas/src/main/scala/de/tu_darmstadt/veritas/VerificationInfrastructure/lemmagen/clever/construction/LemmaGraphConstructor.scala
@@ -2,11 +2,9 @@ package de.tu_darmstadt.veritas.VerificationInfrastructure.lemmagen.clever.const
 
 import de.tu_darmstadt.veritas.VerificationInfrastructure.lemmagen.util.{Constraint, StrategyHelpers}
 import de.tu_darmstadt.veritas.VerificationInfrastructure.lemmagen.clever.{AnnotatedLemma, Hints, RefinementNode}
-import de.tu_darmstadt.veritas.VerificationInfrastructure.lemmagen.{LemmaGenSpecEnquirer, Refinement}
+import de.tu_darmstadt.veritas.VerificationInfrastructure.lemmagen.{Lemma, LemmaGenSpecEnquirer, Refinement}
 import de.tu_darmstadt.veritas.backend.ast.MetaVar
-import de.tu_darmstadt.veritas.backend.ast.function.{FunctionExpMeta, FunctionMeta}
-
-import scala.collection.mutable
+import de.tu_darmstadt.veritas.backend.ast.function.{FunctionDef, FunctionExpMeta, FunctionMeta}
 
 trait LemmaGraphConstructor extends GraphConstructor with StrategyHelpers {
   import de.tu_darmstadt.veritas.VerificationInfrastructure.lemmagen.util.Query._
@@ -18,44 +16,37 @@ trait LemmaGraphConstructor extends GraphConstructor with StrategyHelpers {
   def restrictableVariables(node: RefinementNode): Set[MetaVar]
 
   def generateEquations(node: RefinementNode): Set[Refinement] = {
-    val restrictable = restrictableVariables(node)
-    val partitioned = restrictable.groupBy(_.sortType)
-    var restrictions = new mutable.ListBuffer[Refinement]()
-    for((typ, metaVars) <- partitioned) {
-      if(metaVars.size > 1) {
-        val equals = metaVars.subsets.filter(_.size == 2)
-        for(equal <- equals) {
-          if(!equal.subsetOf(invocationArguments.toSet))
-            restrictions += Refinement.Equation(equal.head, equal.tail.head)
-        }
-      }
-    }
-    restrictions.toSet
+    val partitioned = restrictableVariables(node).groupBy(_.sortType)
+    partitioned.flatMap {
+      case (_, metaVars) =>
+        for(equal <- metaVars.subsets.filter(_.size == 2) if !equal.subsetOf(invocationArguments.toSet))
+          yield Refinement.Equation(equal.head, equal.tail.head)
+    }.toSet
+  }
+
+  def possibleRefinements(lemma: Lemma, function: FunctionDef): Seq[Refinement] = {
+    if(function.outType.name == "Bool")
+      selectPredicate(lemma, function)
+    else
+      selectSuccessfulApplication(lemma, function,
+        Constraint.preferBound(function.inTypes),
+        Constraint.preferBound(function.successfulOutType))
   }
 
   def generateApplications(node: RefinementNode): Set[Refinement] = {
     val invocationTypes = invocationArguments.map(_.sortType)
     val notConstrainedYet: Set[FunctionExpMeta] = (node.lemma.boundVariables -- node.constrainedVariables).map(FunctionMeta(_))
+    val postVars: Set[FunctionExpMeta] = node.postVariables.map(FunctionMeta(_))
     val relevantFunctions = problem.enquirer.staticFunctions.filter(_.inTypes.intersect(invocationTypes).nonEmpty)
-    relevantFunctions.flatMap(staticFn =>
-      if (staticFn.signature.out.name == "Bool") {
-        var refinements = selectPredicate(node.lemma, staticFn)
-        refinements = refinements.filter(r => r.arguments.toSet.intersect(notConstrainedYet).nonEmpty)
-        // do not want refinements whose in arguments contain post variables
-        val postVars: Set[FunctionExpMeta] = node.postVariables.map(FunctionMeta(_))
-        refinements = refinements.filterNot(r => r.arguments.exists(arg => postVars.contains(arg)))
-        refinements
-      } else {
-        var refinements = selectSuccessfulApplication(node.lemma, staticFn,
-          Constraint.preferBound(staticFn.inTypes),
-          Constraint.preferBound(staticFn.successfulOutType))
-        refinements = refinements.filter(r => r.arguments.toSet.intersect(notConstrainedYet).nonEmpty)
-        // do not want refinements whose in arguments contain post variables
-        val postVars: Set[FunctionExpMeta] = node.postVariables.map(FunctionMeta(_))
-        refinements = refinements.filterNot(r => r.arguments.exists(arg => postVars.contains(arg)))
-        refinements
+    relevantFunctions.flatMap { staticFn =>
+      possibleRefinements(node.lemma, staticFn).filter { refinement =>
+        val arguments = refinement match {
+          case Refinement.Predicate(_, a) => a.toSet
+          case Refinement.SuccessfulApplication(_, a, _) => a.toSet
+        }
+        arguments.intersect(notConstrainedYet).nonEmpty && arguments.intersect(postVars).isEmpty
       }
-    )
+     }
   }
 
   override def expand(node: RefinementNode): Set[Refinement] = {
